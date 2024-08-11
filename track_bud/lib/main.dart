7cbf26b14bce545cc40804af6f7d72c06df39f30
Lenz <laurenz.ueckert@gmail.com>
2024-08-11 23:47:24 +0200

    clean up waring messages and change ios version

--- track_bud/lib/main.dart
+++ track_bud/lib/main.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:track_bud/services/auth/auth_gate.dart';
 import 'package:track_bud/utils/color_theme.dart';
-import 'package:track_bud/views/at_signup/forgot_password_screen.dart';
-import 'package:track_bud/views/at_signup/login.dart';
-import 'package:track_bud/views/at_signup/onboarding.dart';
 import 'services/firebase_options.dart';
-import 'views/at_signup/signup.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,10 +18,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'TrackBud',
-    theme: ThemeClass.lightTheme,
-    home: AuthGate(),
-);
-
+      title: 'TrackBud',
+      theme: ThemeClass.lightTheme,
+      home: AuthGate(),
+    );
   }
 }
