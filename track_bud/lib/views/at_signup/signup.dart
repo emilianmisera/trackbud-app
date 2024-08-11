bfef76db824a5b4e0e6beb12c8a28666d844e23f
Emilian <emilian.mi@web.de>
2024-08-11 22:37:10 +0200

    duplicate sign in screen and modified to sign up

--- track_bud/lib/views/at_signup/signup.dart
+++ track_bud/lib/views/at_signup/signup.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfield_widget.dart';
@@ -25,27 +24,39 @@ class _SignUpScreenState extends State<SignUpScreen> {
           crossAxisAlignment: CrossAxisAlignment.start, //alignment to left
           children: [
             Text(
-              AppString.signIn,
+              AppString.signUp,
               style: CustomTextStyle.headingStyle,
             ),
             SizedBox(
               height: CustomPadding.mediumSpace,
             ),
             Text(
-              AppString.signInDescribtion,
+              AppString.signUpDescribtion,
               style: CustomTextStyle.hintStyleDefault,
             ),
             SizedBox(
               height: CustomPadding.defaultSpace,
             ),
             Textfield(
-                name: AppString.email, hintText: AppString.hintEmail), //email
+                name: AppString.firstName, hintText: AppString.hintFirstName), //email
+            SizedBox(
+              height: CustomPadding.defaultSpace,
+            ),
+            Textfield(
+                name: AppString.email,
+                hintText: AppString.hintEmail),
             SizedBox(
               height: CustomPadding.defaultSpace,
             ),
             Textfield(
                 name: AppString.password,
-                hintText: AppString.hintPassword), //password
+                hintText: AppString.hintPassword),
+            SizedBox(
+              height: CustomPadding.defaultSpace,
+            ),
+            Textfield(
+                name: AppString.confirmPassword,
+                hintText: AppString.confirmPassword), //password
             SizedBox(
               height: CustomPadding.bigSpace,
             ),
@@ -53,65 +64,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
               //sign in button
               onPressed: () {},
               child: Text(
-                AppString.signIn,
-              ),
-            ),
-            SizedBox(
-              height: CustomPadding.bigSpace,
-            ),
-            Row(
-              // Divider
-              children: [
-                Expanded(
-                  child: Container(
-                      margin: const EdgeInsets.only(
-                          right: CustomPadding.mediumSpace),
-                      child: Divider(
-                        color: CustomColor.grey,
-                      )),
-                ),
-                Text(
-                  AppString.or,
-                  style: CustomTextStyle.hintStyleMedium,
-                ),
-                Expanded(
-                  child: Container(
-                      margin: const EdgeInsets.only(
-                          left: CustomPadding.mediumSpace),
-                      child: Divider(
-                        color: CustomColor.grey,
-                      )),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: CustomPadding.bigSpace,
-            ),
-            Shadow(
-              // Google Sign In
-              child: TextButton.icon(
-                onPressed: () {},
-                label: Text(AppString.signInWithGoogle),
-                icon: SvgPicture.asset(AssetImport.googleLogo),
-              ),
-            ),
-            SizedBox(
-              height: CustomPadding.defaultSpace,
-            ),
-            Shadow(
-              // Apple Sign In
-              child: TextButton.icon(
-                onPressed: () {},
-                label: Text(AppString.signInWithApple),
-                icon: SvgPicture.asset(AssetImport.appleLogo),
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.resolveWith<Color>(
-                    (states) => CustomColor.black,
-                  ),
-                  foregroundColor: WidgetStateProperty.resolveWith<Color>(
-                    (states) => CustomColor.white,
-                  ),
-                ),
+                AppString.signUp,
               ),
             ),
             SizedBox(
@@ -120,14 +73,14 @@ class _SignUpScreenState extends State<SignUpScreen> {
             Row( // Redirection to sign up page if user doesn't have an account
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(AppString.newHere, style: CustomTextStyle.hintStyleMedium),
+                Text(AppString.notNew, style: CustomTextStyle.hintStyleMedium),
                 SizedBox(
                   width: CustomPadding.smallSpace,
                 ),
                 GestureDetector(
                   onTap: () {},
                   child: Text(
-                    AppString.signUp,
+                    AppString.signIn,
                     style: TextStyle(
                       fontSize: CustomTextStyle.fontSizeDefault,
                       fontWeight: CustomTextStyle.fontWeightMedium,
