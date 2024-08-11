0397edead1bb6fa582d0e595b3ad7e4e568c25a5
Emilian <emilian.mi@web.de>
2024-08-11 13:05:51 +0200

    add logo file

--- track_bud/lib/main.dart
+++ track_bud/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:track_bud/views/at_signup/onboarding.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,12 +16,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const OnboardingScreen();
   }
 }
