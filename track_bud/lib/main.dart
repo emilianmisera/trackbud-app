14c33a003ac16e6e1baa8018e356fe5743b1f9bd
Emilian <167193316+emilianmisera@users.noreply.github.com>
2024-08-11 17:18:18 +0200

    Revert "Onboarding screen"

--- track_bud/lib/main.dart
+++ track_bud/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:track_bud/utils/color_theme.dart';
-import 'package:track_bud/views/at_signup/onboarding.dart';
 import 'package:track_bud/services/auth/auth_gate.dart';
 import 'services/firebase_options.dart';
 
@@ -18,11 +16,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    title: 'TrackBud',
-    theme: ThemeClass.lightTheme,
-    home: AuthGate(),
-);
-
+    return const MaterialApp(home: AuthGate());
   }
 }
--- track_bud/lib/utils/strings.dart
+++
@@ -1,7 +0,0 @@
-class AppString{
-  static String onboardingTitle = "Verwalte deine Finanzen einfach und effizient an einem Ort.";
-  static String onboardingDescribtion = "Tracke mühelos deine Ein- und Ausgaben und behalte stets den Überblick über deine Schulden und Rückzahlungen von Freunden.";
-  static String signIn = "Anmelden";
-  static String signUp = "Registrieren";
-
-}
