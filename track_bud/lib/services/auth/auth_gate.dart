fb1d5a6a5175d5dd860e1e96f2f421a23761a3b9
LaurenzUeckert <laurenz.ueckert@gmail.com>
2024-08-11 16:01:18 +0200

    Create auth_gate.dart

---
+++ track_bud/lib/services/auth/auth_gate.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:track_bud/views/at_signup/login_page.dart';
+import 'package:track_bud/views/nav_pages/overview_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          //user is logged in
+          if (snapshot.hasData) {
+            return const OverviewPage();
+          }
+
+          //user is not logged in
+          else {
+            return const LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
