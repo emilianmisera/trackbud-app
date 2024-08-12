359ff1645c3506f5bc165973b189cda73cd4746a
Emilian <emilian.mi@web.de>
2024-08-12 21:42:30 +0200

    UI for account_settings_page

--- track_bud/lib/views/subpages/account_settings_screen.dart
+++ track_bud/lib/views/subpages/account_settings_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:track_bud/utils/buttons_widget.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/strings.dart';
 
 class AccountSettingsScreen extends StatefulWidget {
   const AccountSettingsScreen({super.key});
@@ -8,8 +11,84 @@ class AccountSettingsScreen extends StatefulWidget {
 }
 
 class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
+  bool isActive = false;
+
+  void _toggleSwitch(bool value) {
+    // makes switch widget active when bool == true
+    setState(() {
+      isActive = !isActive;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppString.accAdjustments,
+            style: CustomTextStyle.regularStyleMedium),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height *
+                      CustomPadding.topSpaceAuth -
+                  Constants.defaultAppBarHeight,
+              left: CustomPadding.defaultSpace,
+              right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppString.budget,
+                style: CustomTextStyle.regularStyleMedium,
+              ),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              AccAdjustmentButton(
+                  //BankAccount
+                  icon: AssetImport.target,
+                  name: AppString.changeBankAcc,
+                  onPressed: () {}),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              AccAdjustmentButton(
+                  // Busget Goal
+                  icon: AssetImport.target,
+                  name: AppString.changeBudgetGoal,
+                  onPressed: () {}),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              AccAdjustmentWidget(
+                // Currency
+                icon: AssetImport.settings,
+                name: AppString.changeCurrency,
+                widget: CustomDropDown(), // Dropdown for changing currency
+              ),
+              SizedBox(height: CustomPadding.defaultSpace),
+              Text(
+                AppString.appearance,
+                style: CustomTextStyle.regularStyleMedium,
+              ),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              AccAdjustmentWidget(
+                // DarkMode
+                icon: AssetImport.mode,
+                name: AppString.darkMode,
+                widget: Switch( // Switch Widget 
+                  value: isActive,
+                  onChanged: _toggleSwitch,
+                  activeColor: CustomColor.bluePrimary,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
