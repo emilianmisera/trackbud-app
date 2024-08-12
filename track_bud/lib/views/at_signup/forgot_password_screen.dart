95fb848151e51dad4f2f082a1dd72f04a8ee680a
Lenz <laurenz.ueckert@gmail.com>
2024-08-12 10:45:32 +0200

    add password reset functionality to screen (hard coded strings)

--- track_bud/lib/views/at_signup/forgot_password_screen.dart
+++ track_bud/lib/views/at_signup/forgot_password_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:track_bud/services/auth/auth_service.dart'; // Import your AuthService
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfield_widget.dart';
@@ -12,6 +14,7 @@ class ForgotPasswordScreen extends StatefulWidget {
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   final TextEditingController _emailController = TextEditingController();
+  final AuthService _authService = AuthService(); // Create an instance of AuthService
 
   @override
   Widget build(BuildContext context) {
@@ -51,8 +54,42 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
                 height: CustomPadding.bigSpace,
               ),
               ElevatedButton(
-                //sign in button
-                onPressed: () {},
+                onPressed: () async {
+                  // Retrieve email input from the text controller
+                  String email = _emailController.text.trim();
+
+                  // Validate email input
+                  if (email.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Please enter your email."),
+                      ),
+                    );
+                    return;
+                  }
+
+                  try {
+                    // Attempt to send a password reset email
+                    await _authService.sendPasswordResetEmail(email);
+
+                    // Show success message
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Password reset email sent."),
+                      ),
+                    );
+
+                    // Optionally navigate back to login screen
+                    Navigator.of(context).pop();
+                  } on FirebaseAuthException catch (e) {
+                    // Handle error and show error message
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Error: ${e.message}"),
+                      ),
+                    );
+                  }
+                },
                 child: Text(
                   AppString.continueText,
                 ),
