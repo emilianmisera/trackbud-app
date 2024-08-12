416d8040a66dd41de5bcd68d096eeeb4ed1ad33f
LaurenzUeckert <laurenz.ueckert@gmail.com>
2024-08-12 14:56:10 +0200

    add name to auth_service parameters

--- track_bud/lib/views/at_signup/signup_screen.dart
+++ track_bud/lib/views/at_signup/signup_screen.dart
@@ -54,8 +54,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
     try {
       // Attempt to create a new user using the AuthService
-      UserCredential userCredential =
-          await _authService.createUserWithEmailAndPassword(email, password);
+      UserCredential userCredential = await _authService
+          .createUserWithEmailAndPassword(email, password, name);
 
       // Handle successful sign-up (navigate to home screen or show success message)
       ScaffoldMessenger.of(context).showSnackBar(
