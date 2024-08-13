cb6aae8ef6f2809d6fdb395719b9409a9ef4bbbf
Emilian <emilian.mi@web.de>
2024-08-13 20:51:26 +0200

    add strings

--- track_bud/lib/utils/strings.dart
+++ track_bud/lib/utils/strings.dart
@@ -78,4 +78,8 @@ class AppString {
   //change bankaccount
   static String changeBankAccHeading = "Ändere dein Kontostand";
   static String changeBankAccDescribtion = "Stimmt dein angegebener Kontostand nicht mehr überein? Ändere es, um die alle Funktionen vollständig nutzen zu können!";
+
+  //change budgetgoal
+  static String changeBudgetGoalHeading= "Ändere dein Budget Ziel";
+  static String changeBudgetGoalDescribtion = "Zu sparsam oder doch eher zu optimistisch gewesen? Bearbeite dein Budgetziel.";
 }
