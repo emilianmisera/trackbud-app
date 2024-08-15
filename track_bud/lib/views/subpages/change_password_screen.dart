b5bdc556365f6f482ac51b128b1e900703966da9
Emilian <emilian.mi@web.de>
2024-08-15 10:49:28 +0200

    create ChangePasswordScreen

---
+++ track_bud/lib/views/subpages/change_password_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/strings.dart';
+import 'package:track_bud/utils/textfield_widget.dart';
+
+class ChangePasswordScreen extends StatefulWidget {
+  const ChangePasswordScreen({super.key});
+
+  @override
+  State<ChangePasswordScreen> createState() => _ChangePasswordScreenState();
+}
+
+class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          // Padding adds spacing around the content inside the screen.
+          padding: EdgeInsets.only(
+            top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace -
+                Constants
+                    .defaultAppBarHeight, // Top padding based on screen height
+            left: CustomPadding.defaultSpace, // Left padding
+            right: CustomPadding.defaultSpace, // Right padding
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextfield(
+                  name: AppString.newPassword,
+                  hintText: AppString.hintPassword,
+                  controller: _passwordController),
+              SizedBox(height: CustomPadding.defaultSpace),
+              // Confirm Password text field
+              CustomTextfield(
+                  name: AppString.confirmPassword,
+                  hintText: AppString.confirmNewPasswort,
+                  controller: _confirmPasswordController),
+            ],),),)
+    );
+  }
+}
