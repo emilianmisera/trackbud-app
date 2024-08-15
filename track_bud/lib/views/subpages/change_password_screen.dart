4255a3cae4e893eb0da1b8f9f740fd5148ace4c0
Emilian <emilian.mi@web.de>
2024-08-15 11:18:40 +0200

    add Textfields + forgot password + bottomSheet

--- track_bud/lib/views/subpages/change_password_screen.dart
+++ track_bud/lib/views/subpages/change_password_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfield_widget.dart';
+import 'package:track_bud/views/at_signup/forgot_password_screen.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({super.key});
@@ -11,8 +12,11 @@ class ChangePasswordScreen extends StatefulWidget {
 }
 
 class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+  final TextEditingController _currentPasswordController =
+      TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +35,46 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Text(
+                AppString.changePassword, // The heading text
+                style: CustomTextStyle
+                    .headingStyle, // The text style for the heading.
+              ),
+              SizedBox(
+                height: CustomPadding
+                    .mediumSpace, // Adds vertical space between the heading and the next element.
+              ),
+              Text(
+                AppString.changePasswordDesscribtion, // The description text
+                style: CustomTextStyle
+                    .hintStyleDefault, // The text style for the description.
+              ),
+              SizedBox(
+                height: CustomPadding
+                    .bigSpace, // Adds more vertical space before the next element.
+              ),
+              CustomTextfield(
+                  name: AppString.currentPassword,
+                  hintText: AppString.currentPasswordHint,
+                  controller: _currentPasswordController),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: GestureDetector(
+                  // forgot Password
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ForgotPasswordScreen()));
+                  },
+                  child: Text(
+                    AppString.forgotPassword,
+                    style: CustomTextStyle.hintStyleMedium,
+                  ),
+                ),
+              ),
+              SizedBox(height: CustomPadding.defaultSpace),
               CustomTextfield(
                   name: AppString.newPassword,
                   hintText: AppString.hintPassword,
@@ -41,7 +85,25 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                   name: AppString.confirmPassword,
                   hintText: AppString.confirmNewPasswort,
                   controller: _confirmPasswordController),
-            ],),),)
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: 
+        Container(
+          margin: EdgeInsets.only(
+            bottom: MediaQuery.sizeOf(context).height *
+                CustomPadding
+                    .bottomSpace, // Bottom margin based on screen height
+            left: CustomPadding.defaultSpace, // Left margin
+            right: CustomPadding.defaultSpace, // Right margin
+          ),
+          width: MediaQuery.of(context).size.width,
+          child: ElevatedButton(
+            onPressed: () async {},
+            child: Text(AppString.save),
+          ),
+        ),
     );
   }
 }
