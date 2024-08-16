007e4848540fa3ef4e25d51d182e40e9532f8289
Emilian <emilian.mi@web.de>
2024-08-16 21:30:13 +0200

    connect balance user data to ui

--- track_bud/lib/views/nav_pages/analysis_screen.dart
+++ track_bud/lib/views/nav_pages/analysis_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:track_bud/controller/user_controller.dart';
 import 'package:track_bud/utils/buttons_widget.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/information_tiles.dart';
@@ -11,6 +13,38 @@ class AnalysisScreen extends StatefulWidget {
 }
 
 class _AnalysisScreenState extends State<AnalysisScreen> {
+  final UserController _userController = UserController();
+  double _currentBalance = 0;
+
+
+
+  Future<void> _loadCurrentBankAccountInfo() async {
+    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+    if (userId.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Benutzer nicht angemeldet."),
+        ),
+      );
+      return;
+    }
+
+    try {
+      double currentBalance =
+          await _userController.getBankAccountBalance(userId);
+          _currentBalance = currentBalance;
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Fehler beim Laden des Bankkontostands: $e"),
+        ),
+      );
+    }
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +59,8 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                InfoTile(title: 'title', amount: 'amount', color: CustomColor.bluePrimary),
+                InfoTile(title: 'title', amount: '${_currentBalance
+          .toStringAsFixed(2)}', color: CustomColor.bluePrimary),
                 SizedBox(height: CustomPadding.mediumSpace,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -34,6 +69,8 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
                     CustomDropDown(list: ['Ausgaben', 'Einkommen'], width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace,height: 105,),
                   ],
                 ),
+                SizedBox(height: CustomPadding.defaultSpace,),
+
               ],
             ),
         ),
