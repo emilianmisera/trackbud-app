4a3fb05b16e867c0ab46d79d0c1eacf7c2353d4d
Emilian <emilian.mi@web.de>
2024-08-16 21:17:50 +0200

    implement upper body of Analysis

--- track_bud/lib/views/nav_pages/analysis_screen.dart
+++ track_bud/lib/views/nav_pages/analysis_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:track_bud/utils/buttons_widget.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/information_tiles.dart';
 
 class AnalysisScreen extends StatefulWidget {
   const AnalysisScreen({super.key});
@@ -11,10 +14,30 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Analyse'),
+      body: SingleChildScrollView(
+        child: Padding(
+            // spacing between content and screen
+            padding: EdgeInsets.only(
+                top: MediaQuery.sizeOf(context).height *
+                    CustomPadding.topSpace,
+                left: CustomPadding.defaultSpace,
+                right: CustomPadding.defaultSpace),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InfoTile(title: 'title', amount: 'amount', color: CustomColor.bluePrimary),
+                SizedBox(height: CustomPadding.mediumSpace,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InfoTile(title: 'title', amount: 'amount', color: CustomColor.red, width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace),
+                    CustomDropDown(list: ['Ausgaben', 'Einkommen'], width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace,height: 105,),
+                  ],
+                ),
+              ],
+            ),
+        ),
       ),
-      body: Placeholder(),
     );
   }
 }
