a09aed723848afbd63f5f6bfcc777d8502745713
Emilian <emilian.mi@web.de>
2024-08-17 23:18:34 +0200

    adjust code to change chart based on selected option

--- track_bud/lib/views/nav_pages/analysis_screen.dart
+++ track_bud/lib/views/nav_pages/analysis_screen.dart
@@ -17,13 +17,13 @@ class AnalysisScreen extends StatefulWidget {
 class _AnalysisScreenState extends State<AnalysisScreen> {
   final UserController _userController = UserController();
   double _currentBalance = 0.00;
+  String _selectedOption = 'Ausgaben'; //default option
 
- @override
+  @override
   void initState() {
     super.initState();
     _loadCurrentBankAccountInfo(); // Load bank account info when screen is initialized
   }
-
 
   Future<void> _loadCurrentBankAccountInfo() async {
     final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
@@ -40,9 +40,9 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
     try {
       double currentBalance =
           await _userController.getBankAccountBalance(userId);
-          setState(() {
-            _currentBalance = currentBalance;
-          });
+      setState(() {
+        _currentBalance = currentBalance;
+      });
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -52,41 +52,67 @@ class _AnalysisScreenState extends State<AnalysisScreen> {
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-            // spacing between content and screen
-            padding: EdgeInsets.only(
-                top: MediaQuery.sizeOf(context).height *
-                    CustomPadding.topSpace,
-                left: CustomPadding.defaultSpace,
-                right: CustomPadding.defaultSpace),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InfoTile(title: AppString.balance, amount: '${_currentBalance
-          .toStringAsFixed(2)}', color: CustomColor.bluePrimary),
-                SizedBox(height: CustomPadding.mediumSpace,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InfoTile(title: 'title', amount: 'amount', color: CustomColor.red, width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace),
-                    CustomDropDown(list: ['Ausgaben', 'Einkommen'], width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace,height: 105,),
-                  ],
-                ),
-                SizedBox(height: CustomPadding.defaultSpace,),
-                ChartTile(chartChild: DonutChart(),),
-                SizedBox(height: CustomPadding.defaultSpace,),
-                Text('Verlauf', style: CustomTextStyle.regularStyleMedium,),
-                SizedBox(height: CustomPadding.mediumSpace,),
-                TransactionTile(),
-                
-              ],
-            ),
+          // spacing between content and screen
+          padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
+              left: CustomPadding.defaultSpace,
+              right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InfoTile(
+                  title: AppString.balance,
+                  amount: '${_currentBalance.toStringAsFixed(2)}',
+                  color: CustomColor.bluePrimary),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InfoTile(
+                      title: 'title',
+                      amount: 'amount',
+                      color: CustomColor.red,
+                      width: MediaQuery.sizeOf(context).width / 2 -
+                          Constants.infoTileSpace),
+                  CustomDropDown(
+                    list: ['Ausgaben', 'Einkommen'],
+                    width: MediaQuery.sizeOf(context).width / 2 -
+                        Constants.infoTileSpace,
+                    height: 105,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedOption = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: CustomPadding.defaultSpace,
+              ),
+              ChartTile(
+                chartChild: ChartTile(chartChild: DonutChart(selectedOption: _selectedOption)),
+              ),
+              SizedBox(
+                height: CustomPadding.defaultSpace,
+              ),
+              Text(
+                'Verlauf',
+                style: CustomTextStyle.regularStyleMedium,
+              ),
+              SizedBox(
+                height: CustomPadding.mediumSpace,
+              ),
+              TransactionTile(),
+            ],
+          ),
         ),
       ),
     );
