f4fdca068a5f01d2f6890203d515e9e2bfa91984
Emilian <emilian.mi@web.de>
2024-08-17 19:31:26 +0200

    add more comments

--- track_bud/lib/utils/charts.dart
+++ track_bud/lib/utils/charts.dart
@@ -4,6 +4,7 @@ import 'package:track_bud/utils/buttons_widget.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 
+// Background of Charts
 class ChartTile extends StatelessWidget {
   final Widget chartChild;
   const ChartTile({super.key, required this.chartChild});
@@ -12,7 +13,8 @@ class ChartTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.sizeOf(context).width,
-      padding: EdgeInsets.only(top: CustomPadding.bigSpace, bottom: CustomPadding.defaultSpace),
+      padding: EdgeInsets.only(
+          top: CustomPadding.bigSpace, bottom: CustomPadding.defaultSpace),
       decoration: BoxDecoration(
         color: CustomColor.white,
         borderRadius: BorderRadius.circular(Constants.buttonBorderRadius),
@@ -59,32 +61,38 @@ class _DonutChartState extends State<DonutChart> {
     ),
     PieChartSectionData(
       color: CustomColor.unterkunft,
-      value: 40, //TODO: insert overall Amount of Unterkunft category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Unterkunft category Transaction here
       title: AppString.unterkunft,
     ),
     PieChartSectionData(
       color: CustomColor.restaurant,
-      value: 40, //TODO: insert overall Amount of Restaurant category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Restaurant category Transaction here
       title: AppString.restaurants,
     ),
     PieChartSectionData(
       color: CustomColor.mobility,
-      value: 40, //TODO: insert overall Amount of Mobility category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Mobility category Transaction here
       title: AppString.mobility,
     ),
     PieChartSectionData(
       color: CustomColor.entertainment,
-      value: 40, //TODO: insert overall Amount of Entertainment category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Entertainment category Transaction here
       title: AppString.entertainment,
     ),
     PieChartSectionData(
       color: CustomColor.geschenk,
-      value: 40, //TODO: insert overall Amount of Geschenk category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Geschenk category Transaction here
       title: AppString.geschenke,
     ),
     PieChartSectionData(
       color: CustomColor.sonstiges,
-      value: 40, //TODO: insert overall Amount of Lebensmittel category Transaction here
+      value:
+          40, //TODO: insert overall Amount of Lebensmittel category Transaction here
       title: AppString.sonstiges,
     ),
   ];
@@ -112,33 +120,37 @@ class _DonutChartState extends State<DonutChart> {
     return Column(
       children: [
         // Pie chart
-        AspectRatio(
-          aspectRatio: 1.3,
+        AspectRatio( // control the shape of the container that holds the pie chart
+          aspectRatio: 1.3, // Set the aspect ratio of the chart container
           child: PieChart(
             PieChartData(
-              borderData: FlBorderData(show: false),
-              sectionsSpace: 0,
-              centerSpaceRadius: 80,
-              sections: showingSections(),
+              borderData:
+                  FlBorderData(show: false), // Hide the border of the chart
+              sectionsSpace: 0, // No space between pie sections
+              centerSpaceRadius:
+                  80, // Set the radius of the empty space in the center
+              sections:
+                  showingSections(), // Get the sections data
               pieTouchData: PieTouchData(
                 // Handle touch events on the pie chart
                 touchCallback: (FlTouchEvent event, pieTouchResponse) {
                   if (event is FlTapUpEvent) {
-                    // Nur auf das TouchUp-Event reagieren
+                    // Only react to the TouchUp event
                     setState(() {
                       if (pieTouchResponse == null ||
                           pieTouchResponse.touchedSection == null) {
-                        return;
+                        return; // Exit if no valid touch response
                       }
                       final touchedIndex =
                           pieTouchResponse.touchedSection!.touchedSectionIndex;
                       if (touchedIndex < 0 || touchedIndex >= sections.length) {
-                        return;
+                        return; // Exit if touched index is out of range
                       }
                       if (selectedIndex == touchedIndex) {
-                        selectedIndex = null;
+                        selectedIndex = null; // Deselect if already selected
                       } else {
-                        selectedIndex = touchedIndex;
+                        selectedIndex =
+                            touchedIndex; // Select the touched section
                       }
                     });
                   }
@@ -147,7 +159,9 @@ class _DonutChartState extends State<DonutChart> {
             ),
           ),
         ),
-        SizedBox(height: CustomPadding.defaultSpace,),
+        SizedBox(
+          height: CustomPadding.defaultSpace,
+        ),
         // Category names display
         Column(
           children: selectedIndex != null && selectedIndex! < sections.length
@@ -160,23 +174,20 @@ class _DonutChartState extends State<DonutChart> {
                   )
                 ]
               : // Display all category names if no category is selected
-              sections
-                  .map(
-                    (section) => Padding(
+              sections.map((section) => Padding(
                         padding: const EdgeInsets.symmetric(vertical: 4.0),
                         child: CategoryTile(
                           color: section.color,
                           title: section.title,
                           percentage: section.value,
-                        )),
-                  )
-                  .toList(),
+                        ))).toList(),
         ),
       ],
     );
   }
 }
 
+// Widget that displays Information about the chart
 class CategoryTile extends StatelessWidget {
   final Color color;
   final String title;
@@ -196,7 +207,7 @@ class CategoryTile extends StatelessWidget {
           borderRadius: BorderRadius.circular(Constants.buttonBorderRadius)),
       child: ListTile(
         // Icon
-        leading: CategoryIcon(color: color, icon: AssetImport.appleLogo),
+        leading: CategoryIcon(color: color, icon: AssetImport.appleLogo), //TODO: Add Icons
         // Title of Transaction
         title: Text(
           title,
@@ -213,14 +224,14 @@ class CategoryTile extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Text(
-              '100€',
+              '100€', //TODO: implement total Amount of category expense/income
               style: CustomTextStyle.regularStyleMedium,
             ),
             SizedBox(
               height: 8,
             ),
             Text(
-              '- Transaktionen',
+              '- Transaktionen', //TODO: implement length of list of amount of transactions of specific categroy
               style: CustomTextStyle.hintStyleDefault
                   .copyWith(fontSize: CustomTextStyle.fontSizeHint),
             ),
