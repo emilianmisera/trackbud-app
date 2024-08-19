fe9160c076438425975c2a7c768d1945e27894ca
Emilian <emilian.mi@web.de>
2024-08-19 17:28:25 +0200

    create SearchBar Widget

--- track_bud/lib/utils/textfield_widget.dart
+++ track_bud/lib/utils/textfield_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:simple_shadow/simple_shadow.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
@@ -69,7 +70,7 @@ class CustomTextfield extends StatelessWidget {
                 fillColor: CustomColor.white,
                 border: OutlineInputBorder(
                   borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(Constants.buttonBorderRadius),
                 ),
               ),
             ),
@@ -134,10 +135,54 @@ class TextFieldAmountOfMoney extends StatelessWidget {
           fillColor: CustomColor.white,
           border: OutlineInputBorder(
             borderSide: BorderSide.none,
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(Constants.buttonBorderRadius),
           ),
         ),
       ),
     );
+  }
+}
+
+class SearchTextfield extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool? autofocus;
+
+  const SearchTextfield({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+    this.autofocus,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomShadow(
+          child: Container(
+            width: double.infinity,
+            height: Constants.height,
+            child: TextFormField(
+              controller: controller,
+              cursorColor: CustomColor.bluePrimary,
+              autofocus: autofocus ?? false,
+              decoration: InputDecoration(
+                prefixIcon: SvgPicture.asset(AssetImport.search, fit: BoxFit.scaleDown,),
+                hintText: hintText,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: CustomPadding.defaultSpace,
+                  vertical: CustomPadding.contentHeightSpace,
+                ),
+                hintStyle: CustomTextStyle.hintStyleDefault,
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                filled: true,
+                fillColor: CustomColor.white,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(Constants.buttonBorderRadius),
+                ),
+              ),
+            ),
+          ),
+        );
   }
 }
