e2aeb4d559847b2ef897a513ceae9b210702670a
Emilian <emilian.mi@web.de>
2024-08-19 16:36:54 +0200

    add debts tile and credit tile

--- track_bud/lib/views/nav_pages/debts_screen.dart
+++ track_bud/lib/views/nav_pages/debts_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/information_tiles.dart';
 
 class DebtsScreen extends StatefulWidget {
   const DebtsScreen({super.key});
@@ -11,10 +13,38 @@ class _DebtsScreenState extends State<DebtsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Schulden'),
-      ),
-      body: Placeholder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          // spacing between content and screen
+          padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
+              left: CustomPadding.defaultSpace,
+              right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  InfoTile(
+                      title: 'Schulden',
+                      amount: 'amount',
+                      color: CustomColor.red,
+                      width: MediaQuery.sizeOf(context).width / 2 -
+                          Constants.infoTileSpace),
+                  InfoTile(
+                      title: 'Guthaben',
+                      amount: 'amount',
+                      color: CustomColor.green,
+                      width: MediaQuery.sizeOf(context).width / 2 -
+                          Constants.infoTileSpace),
+                ],
+              ),
+              SizedBox(
+                height: CustomPadding.defaultSpace,
+              ),
+          ],),),)
     );
   }
 }
