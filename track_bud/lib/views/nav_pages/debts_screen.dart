c9d37c7cd00c009fcd72ddd2cac135e8ed198099
Emilian <emilian.mi@web.de>
2024-08-19 16:46:44 +0200

    add Friends Title + Button

--- track_bud/lib/views/nav_pages/debts_screen.dart
+++ track_bud/lib/views/nav_pages/debts_screen.dart
@@ -13,38 +13,46 @@ class _DebtsScreenState extends State<DebtsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          // spacing between content and screen
-          padding: EdgeInsets.only(
-              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
-              left: CustomPadding.defaultSpace,
-              right: CustomPadding.defaultSpace),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-
-                  InfoTile(
-                      title: 'Schulden',
-                      amount: 'amount',
-                      color: CustomColor.red,
-                      width: MediaQuery.sizeOf(context).width / 2 -
-                          Constants.infoTileSpace),
-                  InfoTile(
-                      title: 'Guthaben',
-                      amount: 'amount',
-                      color: CustomColor.green,
-                      width: MediaQuery.sizeOf(context).width / 2 -
-                          Constants.infoTileSpace),
-                ],
-              ),
-              SizedBox(
-                height: CustomPadding.defaultSpace,
-              ),
-          ],),),)
-    );
+        body: SingleChildScrollView(
+      child: Padding(
+        // spacing between content and screen
+        padding: EdgeInsets.only(
+            top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
+            left: CustomPadding.defaultSpace,
+            right: CustomPadding.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InfoTile(
+                    title: 'Schulden',
+                    amount: 'amount',
+                    color: CustomColor.red,
+                    width: MediaQuery.sizeOf(context).width / 2 -
+                        Constants.infoTileSpace),
+                InfoTile(
+                    title: 'Guthaben',
+                    amount: 'amount',
+                    color: CustomColor.green,
+                    width: MediaQuery.sizeOf(context).width / 2 -
+                        Constants.infoTileSpace),
+              ],
+            ),
+            SizedBox(
+              height: CustomPadding.defaultSpace,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Freunde', style: CustomTextStyle.regularStyleMedium,),
+                GestureDetector(onTap: (){}, child: Text('alle Anzeigen', style: CustomTextStyle.regularStyleMedium.copyWith(color: CustomColor.bluePrimary)),)
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
