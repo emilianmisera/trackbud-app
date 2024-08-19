31e7cc6a57123a495513b15bd27e1a429494789e
Emilian <emilian.mi@web.de>
2024-08-19 17:29:32 +0200

    add AppBar and Searchbar

--- track_bud/lib/views/subpages/your_friends_screen.dart
+++ track_bud/lib/views/subpages/your_friends_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/strings.dart';
+import 'package:track_bud/utils/textfield_widget.dart';
 
 class YourFriendsScreen extends StatefulWidget {
   const YourFriendsScreen({super.key});
@@ -8,8 +12,34 @@ class YourFriendsScreen extends StatefulWidget {
 }
 
 class _YourFriendsScreenState extends State<YourFriendsScreen> {
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            AppString.accAdjustments,
+            style: CustomTextStyle.regularStyleMedium,
+        ),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add, color: CustomColor.bluePrimary, size: 30,))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+        // spacing between content and screen
+        padding: EdgeInsets.only(
+            top: CustomPadding.defaultSpace,
+            left: CustomPadding.defaultSpace,
+            right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchTextfield(hintText: AppString.search, controller: _searchController),
+              SizedBox(height: CustomPadding.defaultSpace,),
+            ],
+          ),
+      ),),
+    );
   }
 }
