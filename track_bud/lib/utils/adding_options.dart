21691a7d7a26117067613dae4b60c065a31bbb2b
Lenz <laurenz.ueckert@gmail.com>
2024-08-22 16:13:01 +0200

    add onButtonPressed Callback for working main Branch

--- track_bud/lib/utils/adding_options.dart
+++ track_bud/lib/utils/adding_options.dart
@@ -30,7 +30,6 @@ class DynamicBottomSheet extends StatelessWidget {
   const DynamicBottomSheet({
     Key? key,
     required this.child,
-
     this.initialChildSize = 0.62,
     this.minChildSize = 0.3,
     this.maxChildSize = 0.95,
@@ -82,7 +81,6 @@ class DynamicBottomSheet extends StatelessWidget {
                         CustomPadding.bottomSpace),
                 child: ElevatedButton(
                     onPressed: () {
-
                       onButtonPressed();
                     },
                     child: Text(buttonText)),
@@ -132,7 +130,6 @@ class _AddTransactionState extends State<AddTransaction> {
   String _getAmountPrefix() {
     return _currentSegment == 0 ? '–' : '+';
   }
-
 
   TransactionModel _getTransactionFromForm() {
     final String transactionId = _uuid.v4();
@@ -350,7 +347,9 @@ class AddSplit extends StatefulWidget {
   final bool? isGroup;
   const AddSplit({
     Key? key,
-    this.list, this.friendName, this.isGroup,
+    this.list,
+    this.friendName,
+    this.isGroup,
   }) : super(key: key);
 
   @override
@@ -359,10 +358,13 @@ class AddSplit extends StatefulWidget {
 
 class _AddSplitState extends State<AddSplit> {
   int _currentSegment = 0; // 0 for user, 1 for friend
-  final TextEditingController _titleController = TextEditingController(); // title input
-  final TextEditingController _amountController = TextEditingController(); // amount input
+  final TextEditingController _titleController =
+      TextEditingController(); // title input
+  final TextEditingController _amountController =
+      TextEditingController(); // amount input
 
-  SplitMethod _selectedSplitMethod = SplitMethod.equal; // equal Split is selected as default
+  SplitMethod _selectedSplitMethod =
+      SplitMethod.equal; // equal Split is selected as default
   double _inputNumber = 0.00; // input Number
 
   @override
@@ -378,118 +380,115 @@ class _AddSplitState extends State<AddSplit> {
     });
   }
 
+  Future<void> _saveNewSplit() async {}
+
   @override
   Widget build(BuildContext context) {
     return DynamicBottomSheet(
       buttonText: AppString.addSplit,
-      initialChildSize: 0.80,
+      initialChildSize: 0.62,
       maxChildSize: 0.95,
+      onButtonPressed: () async {
+        await _saveNewSplit();
+        Navigator.pop(context);
+      },
       child: Padding(
         padding: CustomPadding.screenWidth,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Title of the bottom sheet
-            Center(
-              child: Text(
-                AppString.newSplit,
-                style: CustomTextStyle.regularStyleMedium,
-              ),
-            ),
-            SizedBox(
-              height: CustomPadding.defaultSpace,
-            ),
-            // Text field for transaction title
-            CustomTextfield(
-                name: AppString.title,
-                hintText: AppString.hintTitle,
-                controller: _titleController),
-            SizedBox(
-              height: CustomPadding.defaultSpace,
-            ),
-            // Row containing amount and date fields
-            Row(
-              children: [
-                // Amount text field
-                CustomTextfield(
-                  name: AppString.amount,
-                  hintText: AppString.lines,
-                  controller: _amountController,
-                  width: MediaQuery.sizeOf(context).width / 3,
-                  prefix: Text(
-                    '-',
-                    style: CustomTextStyle.titleStyleMedium.copyWith(
-                        fontWeight: CustomTextStyle.fontWeightDefault),
-                  ),
-                  type: TextInputType.numberWithOptions(),
-                ),
-                SizedBox(
-                  width: CustomPadding.defaultSpace,
-                ),
-
-                // Date text field
-                DatePicker()
-              ],
-            ),
-            SizedBox(
-              height: CustomPadding.defaultSpace,
-            ),
-            // choosing who payed
-            Text(
-              AppString.payedBy,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          // Title of the bottom sheet
+          Center(
+            child: Text(
+              AppString.newSplit,
               style: CustomTextStyle.regularStyleMedium,
             ),
-            SizedBox(
-              height: CustomPadding.mediumSpace,
-            ),
-            // Dropdown for selecting person who paid bill
-            CustomDropDown(
-              list: widget.list ?? [
-                'Dir',
-                widget.friendName ?? '**Friend Name**'
-              ],
-              dropdownWidth: MediaQuery.sizeOf(context).width - 32,
-            ),
-            SizedBox(
-              height: CustomPadding.defaultSpace,
-            ),
-            // choose between 3 split options
-            SplitMethodSelector(
-              selectedMethod: _selectedSplitMethod,
-              onSplitMethodChanged: (SplitMethod method) {
-                setState(() {
-                  _selectedSplitMethod = method;
-                });
-              },
-            ),
-            SizedBox(height: CustomPadding.defaultSpace),
-            // first split option (eaul & default)
-            if (_selectedSplitMethod == SplitMethod.equal)
-              EqualSplitWidget(
-                amount: _inputNumber,
-                names: widget.list ?? [
-                'Dir',
-                widget.friendName ?? '**Friend Name**'
-                ],
-                isGroup: widget.isGroup ?? false,
-              ),
-            // second split option (percental)
-            if (_selectedSplitMethod == SplitMethod.percent)
-              PercentalSplitWidget(
-                amount: _inputNumber,
-                names: widget.list ?? [
-                'Dir',
-                widget.friendName ?? '**Friend Name**'
-              ],
+          ),
+          SizedBox(
+            height: CustomPadding.defaultSpace,
+          ),
+          // Text field for transaction title
+          CustomTextfield(
+              name: AppString.title,
+              hintText: AppString.hintTitle,
+              controller: _titleController),
+          SizedBox(
+            height: CustomPadding.defaultSpace,
+          ),
+          // Row containing amount and date fields
+          Row(
+            children: [
+              // Amount text field
+              CustomTextfield(
+                name: AppString.amount,
+                hintText: AppString.lines,
+                controller: _amountController,
+                width: MediaQuery.sizeOf(context).width / 3,
+                prefix: Text(
+                  '-',
+                  style: CustomTextStyle.titleStyleMedium
+                      .copyWith(fontWeight: CustomTextStyle.fontWeightDefault),
                 ),
-            // third split option (by amount)
-            if (_selectedSplitMethod == SplitMethod.amount)
-              ByAmountSplitWidget(names: widget.list ?? [
-                'Dir',
-                widget.friendName ?? '**Friend Name**'
-              ],)
-          ]
-        ),
+                type: TextInputType.numberWithOptions(),
+              ),
+              SizedBox(
+                width: CustomPadding.defaultSpace,
+              ),
+
+              // Date text field
+              DatePicker()
+            ],
+          ),
+          SizedBox(
+            height: CustomPadding.defaultSpace,
+          ),
+          // choosing who payed
+          Text(
+            AppString.payedBy,
+            style: CustomTextStyle.regularStyleMedium,
+          ),
+          SizedBox(
+            height: CustomPadding.mediumSpace,
+          ),
+          // Dropdown for selecting person who paid bill
+          CustomDropDown(
+            list:
+                widget.list ?? ['Dir', widget.friendName ?? '**Friend Name**'],
+            dropdownWidth: MediaQuery.sizeOf(context).width - 32,
+          ),
+          SizedBox(
+            height: CustomPadding.defaultSpace,
+          ),
+          // choose between 3 split options
+          SplitMethodSelector(
+            selectedMethod: _selectedSplitMethod,
+            onSplitMethodChanged: (SplitMethod method) {
+              setState(() {
+                _selectedSplitMethod = method;
+              });
+            },
+          ),
+          SizedBox(height: CustomPadding.defaultSpace),
+          // first split option (eaul & default)
+          if (_selectedSplitMethod == SplitMethod.equal)
+            EqualSplitWidget(
+              amount: _inputNumber,
+              names: widget.list ??
+                  ['Dir', widget.friendName ?? '**Friend Name**'],
+              isGroup: widget.isGroup ?? false,
+            ),
+          // second split option (percental)
+          if (_selectedSplitMethod == SplitMethod.percent)
+            PercentalSplitWidget(
+              amount: _inputNumber,
+              names: widget.list ??
+                  ['Dir', widget.friendName ?? '**Friend Name**'],
+            ),
+          // third split option (by amount)
+          if (_selectedSplitMethod == SplitMethod.amount)
+            ByAmountSplitWidget(
+              names: widget.list ??
+                  ['Dir', widget.friendName ?? '**Friend Name**'],
+            )
+        ]),
       ),
     );
   }
