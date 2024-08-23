f6be65096784639fe0b617edcd6e3025c4b4818b
Lenz <laurenz.ueckert@gmail.com>
2024-08-23 16:49:37 +0200

    add filter to only show users own transactions

--- track_bud/lib/widgets/transaction_list.dart
+++ track_bud/lib/widgets/transaction_list.dart
@@ -1,23 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:track_bud/utils/information_tiles.dart';
 
 class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance
           .collection('transactions')
+          .where('userId', isEqualTo: userId)
           .orderBy('date', descending: true)
           .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
+          print('Ein Fehler ist aufgetreten ${snapshot.error}');
           return Text('Ein Fehler ist aufgetreten');
+          
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
         }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+        return Text('Keine Transaktionen gefunden');
+      }
 
         return ListView(
           shrinkWrap: true,
