7a2064c5d431332727f7808fa9cacf079a966ca1
Emilian <emilian.mi@web.de>
2024-08-27 02:32:40 +0200

    add ExpensesOverview to OverviewScreen

--- track_bud/lib/views/nav_pages/overview_screen.dart
+++ track_bud/lib/views/nav_pages/overview_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/overview.dart';
 
 class OverviewScreen extends StatefulWidget {
   const OverviewScreen({super.key});
@@ -11,10 +13,21 @@ class _OverviewPageState extends State<OverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Übersicht'),
+      body: SingleChildScrollView(
+        child: Padding(
+          // spacing between content and screen
+          padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
+              left: CustomPadding.defaultSpace,
+              right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ExpensesOverview()
+            ],
+          ),
+        ),
       ),
-      body: Placeholder(),
     );
   }
 }
