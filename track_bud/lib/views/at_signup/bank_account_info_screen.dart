255f806007d1fc2b523103e669fa4e4bc80214b5
Emilian <emilian.mi@web.de>
2024-09-03 16:25:36 +0200

    update BankAccountInfoScreen

--- track_bud/lib/views/at_signup/bank_account_info_screen.dart
+++ track_bud/lib/views/at_signup/bank_account_info_screen.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:track_bud/controller/user_controller.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfields/textfield_amount_of_money.dart';
@@ -21,26 +21,32 @@ class _BankAccountInfoScreenState extends State<BankAccountInfoScreen> {
 
   // Add or update user's bank account balance in Firestore
   Future<void> addUserBankAccount(double amount) async {
-    User? user = FirebaseAuth.instance.currentUser;
-    
-    if (user != null) {
+    final String userId = FirebaseAuth.instance.currentUser!.uid;
+    final String amountText = _moneyController.text.trim();
+    final double amount = double.tryParse(amountText) ?? -1;
+    if (amount < 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Ungültiger Betrag."),
+        ),
+      );
+      return;
+    }
+
       try {
-        await FirebaseFirestore.instance.collection("users").doc(user.uid).update({
-          'bankaccount': amount,
-        });
+        debugPrint("trying to update users bankaccount");
+        await UserController().updateBankAccountBalance(userId, amount);
+        debugPrint("successfully updated users bankaccount");
       } catch (e) {
         debugPrint("Error updating bank account: $e");
         // Handle the error
         showDialog(
-        context: context,
-        builder: (context) => const AlertDialog(
-          title: Text('Fehler :('),
-        ));
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('Fehler beim Spiechern der Bankkonto-Informationen $e')
+                ));
       }
-    } else {
-      debugPrint("No authenticated user found");
     }
-  }
 
   // Convert comma-separated string to double
   double? parseCommaDecimal(String value) {
@@ -53,17 +59,16 @@ class _BankAccountInfoScreenState extends State<BankAccountInfoScreen> {
     double? amount = parseCommaDecimal(_moneyController.text);
     if (amount != null) {
       await addUserBankAccount(amount);
-     Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => BudgetGoalScreen()),
-          );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => BudgetGoalScreen()),
+      );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please enter a valid number')),
+        const SnackBar(content: Text('Gib eine valide Zahl an')),
       );
     }
   }
-  
 
   @override
   Widget build(BuildContext context) {
@@ -72,56 +77,34 @@ class _BankAccountInfoScreenState extends State<BankAccountInfoScreen> {
       bottomSheet: Container(
         // Margin is applied to the bottom of the button and the sides for proper spacing.
         margin: EdgeInsets.only(
-          bottom: MediaQuery.sizeOf(context).height *
-              CustomPadding.bottomSpace, // Bottom margin based on screen height
+          bottom: MediaQuery.sizeOf(context).height * CustomPadding.bottomSpace, // Bottom margin based on screen height
           left: CustomPadding.defaultSpace, // Left margin
           right: CustomPadding.defaultSpace, // Right margin
         ),
-        width: MediaQuery.of(context)
-            .size
-            .width, // Set the button width to match the screen width
+        width: MediaQuery.of(context).size.width,
         child: ElevatedButton(
-          onPressed: (){
-            handleSubmission(context);
-          },
-          child: Text(
-            AppTexts.continueText,
-          ),
+          onPressed: () => handleSubmission(context),
+          child: Text(AppTexts.continueText),
         ),
       ),
-      // The main content of the screen is wrapped in a SingleChildScrollView to make it scrollable.
       body: SingleChildScrollView(
         child: Padding(
           // Padding adds spacing around the content inside the screen.
           padding: EdgeInsets.only(
-            top: MediaQuery.sizeOf(context).height *
-                CustomPadding
-                    .topSpaceAuth, // Top padding based on screen height
-            left: CustomPadding.defaultSpace, // Left padding
-            right: CustomPadding.defaultSpace, // Right padding
+            top: MediaQuery.sizeOf(context).height * CustomPadding.topSpaceAuth,
+            left: CustomPadding.defaultSpace,
+            right: CustomPadding.defaultSpace,
           ),
           child: Column(
             // Column to organize the content vertically.
             children: [
-              Text(
-                AppTexts.bankAccInfoHeading, // The heading text
-                style:
-                    TextStyles.headingStyle, // The text style for the heading.
-              ),
-              Gap(
-                CustomPadding
-                    .mediumSpace, // Adds vertical space between the heading and the next element.
-              ),
-              Text(
-                AppTexts.bankAccInfoDescription, // The description text
-                style: TextStyles
-                    .hintStyleDefault, // The text style for the description.
-              ),
-              Gap(
-                CustomPadding
-                    .bigSpace, // Adds more vertical space before the next element.
-              ),
-              // A custom TextField widget for entering the amount of money, using the controller defined above.
+              // The heading text
+              Text(AppTexts.bankAccInfoHeading, style: TextStyles.headingStyle),
+              Gap(CustomPadding.mediumSpace),
+              // The description text
+              Text(AppTexts.bankAccInfoDescription, style: TextStyles.hintStyleDefault),
+              Gap(CustomPadding.bigSpace),
+              // entering the amount of money
               TextFieldAmountOfMoney(controller: _moneyController),
             ],
           ),
