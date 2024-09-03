97b3bf7dfeb5bf8c96b6291d1375340caa715c12
Emilian <emilian.mi@web.de>
2024-09-03 16:54:47 +0200

    update login method

--- track_bud/lib/views/at_signup/login_screen.dart
+++ track_bud/lib/views/at_signup/login_screen.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
@@ -23,16 +21,49 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
 
+  final AuthService _authService = AuthService();
+
+  Future<void> _handleGoogleSignIn() async {
+    try {
+      // Versuche, den Benutzer mit Google anzumelden
+      await _authService.signInWithGoogle(context);
+
+      // Zeige eine Erfolgsmeldung an
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(AppTexts.successfulLogin),
+        ),
+      );
+    } catch (error) {
+      // Zeige eine Fehlermeldung an, falls die Google-Anmeldung fehlgeschlagen ist
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Google-Anmeldung fehlgeschlagen: $error"),
+        ),
+      );
+    }
+  }
+
   void _loginUser() async {
     final AuthService authService = AuthService();
+    String email = _email.text.trim();
+    String password = _password.text.trim();
+
+    if (email.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("${AppTexts.emptyLoginInput}"),
+        ),
+      );
+      return;
+    }
 
     // try login
     try {
       debugPrint('login_screen: _loginUser -> trying to login...');
       await authService.signInWithEmailAndPassword(_email.text, _password.text);
 
-      debugPrint(
-          'login_screen: _loginUser -> sucess! -> redirecting to Overview');
+      debugPrint('login_screen: _loginUser -> sucess! -> redirecting to Overview');
       if (mounted) {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => TrackBud()),
@@ -50,35 +81,6 @@ class _LoginScreenState extends State<LoginScreen> {
     // catch any errors
   }
 
-  Future<Map<String, dynamic>> getCurrentUserData() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      print('User is null');
-      return {};
-    }
-
-    try {
-      print('Attempting to fetch data for user ID: ${user.uid}');
-      DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore
-          .instance
-          .collection('users')
-          .doc(user.uid)
-          .get();
-
-      if (!snapshot.exists) {
-        print('User document does not exist');
-        return {};
-      }
-
-      Map<String, dynamic> data = snapshot.data() ?? {};
-      print('Document data: $data');
-      return data;
-    } catch (e) {
-      print('Error fetching user data: $e');
-      return {};
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,113 +88,77 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Padding(
           // spacing between content and screen
           padding: EdgeInsets.only(
-              top: MediaQuery.sizeOf(context).height *
-                  CustomPadding.topSpaceAuth,
+              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpaceAuth,
               left: CustomPadding.defaultSpace,
               right: CustomPadding.defaultSpace),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start, //alignment to left
             children: [
-              Text(
-                AppTexts.signIn,
-                style: TextStyles.headingStyle,
-              ),
-              Gap(
-                CustomPadding.mediumSpace,
-              ),
-              Text(
-                AppTexts.signInDescription,
-                style: TextStyles.hintStyleDefault,
-              ),
-              Gap(
-                CustomPadding.defaultSpace,
-              ),
+              Text(AppTexts.signIn, style: TextStyles.headingStyle),
+              Gap(CustomPadding.mediumSpace),
+              Text(AppTexts.signInDescription, style: TextStyles.hintStyleDefault),
+              Gap(CustomPadding.defaultSpace),
               CustomTextfield(
                 controller: _email,
                 name: AppTexts.email,
                 hintText: AppTexts.hintEmail,
                 obscureText: false,
               ), //email
-              Gap(
-                CustomPadding.defaultSpace,
-              ),
+              Gap(CustomPadding.defaultSpace),
               CustomTextfield(
                 controller: _password,
                 name: AppTexts.password,
                 hintText: AppTexts.hintPassword,
                 obscureText: true,
               ),
-              Gap(
-                CustomPadding.mediumSpace,
-              ),
+              Gap(CustomPadding.mediumSpace),
               Align(
                 alignment: Alignment.centerRight,
                 child: GestureDetector(
                   // forgot Password
                   onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ForgotPasswordScreen()));
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => ForgotPasswordScreen()));
                   },
-                  child: Text(
-                    AppTexts.forgotPassword,
-                    style: TextStyles.hintStyleMedium,
-                  ),
+                  child: Text(AppTexts.forgotPassword, style: TextStyles.hintStyleMedium),
                 ),
               ),
-              Gap(
-                CustomPadding.bigSpace,
-              ),
+              Gap(CustomPadding.bigSpace),
               ElevatedButton(
                 //sign in button
-                onPressed: () {
-                  _loginUser();
-                },
-                child: Text(
-                  AppTexts.signIn,
-                ),
+                onPressed: () => _loginUser(),
+                child: Text(AppTexts.signIn),
               ),
-              Gap(
-                CustomPadding.bigSpace,
-              ),
+              Gap(CustomPadding.bigSpace),
               Row(
                 // Divider
                 children: [
                   Expanded(
                     child: Container(
-                        margin: const EdgeInsets.only(
-                            right: CustomPadding.mediumSpace),
+                        margin: const EdgeInsets.only(right: CustomPadding.mediumSpace),
                         child: Divider(
                           color: CustomColor.grey,
                         )),
                   ),
-                  Text(
-                    AppTexts.or,
-                    style: TextStyles.hintStyleMedium,
-                  ),
+                  Text(AppTexts.or, style: TextStyles.hintStyleMedium),
                   Expanded(
                     child: Container(
-                        margin: const EdgeInsets.only(
-                            left: CustomPadding.mediumSpace),
+                        margin: const EdgeInsets.only(left: CustomPadding.mediumSpace),
                         child: Divider(
                           color: CustomColor.grey,
                         )),
                   ),
                 ],
               ),
-              Gap(
-                CustomPadding.bigSpace,
-              ),
+              Gap(CustomPadding.bigSpace),
               CustomShadow(
                 // Google Sign In
                 child: TextButton.icon(
-                  onPressed: () async {},
+                  onPressed: () => _handleGoogleSignIn(),
                   label: Text(AppTexts.signInWithGoogle),
                   icon: SvgPicture.asset(AssetImport.googleLogo),
                 ),
               ),
-              Gap(
-                CustomPadding.defaultSpace,
-              ),
+              Gap(CustomPadding.defaultSpace),
               CustomShadow(
                 // Apple Sign In
                 child: TextButton.icon(
@@ -209,21 +175,16 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
-              Gap(
-                CustomPadding.bigSpace,
-              ),
+              Gap(CustomPadding.bigSpace),
               Row(
                 // Redirection to sign up page if user doesn't have an account
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(AppTexts.newHere, style: TextStyles.hintStyleMedium),
-                  Gap(
-                    CustomPadding.smallSpace,
-                  ),
+                  Gap(CustomPadding.smallSpace),
                   GestureDetector(
                     onTap: () {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => SignUpScreen()));
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => SignUpScreen()));
                     },
                     child: Text(
                       AppTexts.signUp,
