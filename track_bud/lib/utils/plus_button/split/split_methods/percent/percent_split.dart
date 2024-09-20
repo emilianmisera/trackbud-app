356a80dee3fe32d38c1d37b3cec21d51d4ad4155
Lenz <laurenz.ueckert@gmail.com>
2024-09-20 23:46:09 +0200

    enable percent split

--- track_bud/lib/utils/plus_button/split/split_methods/percent/percent_split.dart
+++ track_bud/lib/utils/plus_button/split/split_methods/percent/percent_split.dart
@@ -4,17 +4,16 @@ import 'package:track_bud/models/user_model.dart';
 import 'package:track_bud/utils/plus_button/split/split_methods/percent/percent_tile.dart';
 import 'package:track_bud/utils/constants.dart';
 
-/// Widget for displaying percentage split for multiple people
 class PercentalSplitWidget extends StatefulWidget {
-  // Total amount to be split
   final double amount;
-  // List of names of people involved in the split
-final List<UserModel> users; // Change to List<UserModel>
+  final List<UserModel> users;
+  final ValueChanged<List<double>> onAmountsChanged;
 
   const PercentalSplitWidget({
     super.key,
     required this.amount,
     required this.users,
+    required this.onAmountsChanged,
   });
 
   @override
@@ -22,28 +21,51 @@ final List<UserModel> users; // Change to List<UserModel>
 }
 
 class _PercentalSplitWidgetState extends State<PercentalSplitWidget> {
-  // List to store slider values for each person
   late List<double> _sliderValues;
 
   @override
   void initState() {
     super.initState();
-    // Initialize slider values equally among all people
     _sliderValues = List.filled(widget.users.length, 100 / widget.users.length);
+    _updateAmounts();
   }
 
-  // Method to update slider values
   void updateSlider(int index, double value) {
     setState(() {
       if (widget.users.length == 2) {
-        // For two people: automatic adjustment
         _sliderValues[index] = value;
         _sliderValues[1 - index] = 100 - value;
       } else {
-        // For three or more people: independent sliders
-        _sliderValues[index] = value;
+        // For more than 2 users, adjust all sliders proportionally
+        double totalOthers =
+            _sliderValues.reduce((a, b) => a + b) - _sliderValues[index];
+        double newTotal = totalOthers + value;
+        double factor = 100 / newTotal;
+
+        for (int i = 0; i < _sliderValues.length; i++) {
+          if (i == index) {
+            _sliderValues[i] = value * factor;
+          } else {
+            _sliderValues[i] = _sliderValues[i] * factor;
+          }
+        }
       }
+      _updateAmounts();
     });
+  }
+
+  void _updateAmounts() {
+    // Defer the state update to the next frame.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final amounts = calculateAmounts(widget.amount);
+      widget.onAmountsChanged(amounts);
+    });
+  }
+
+  List<double> calculateAmounts(double totalAmount) {
+    return _sliderValues
+        .map((percentage) => (totalAmount * (percentage / 100)))
+        .toList();
   }
 
   @override
@@ -52,11 +74,12 @@ class _PercentalSplitWidgetState extends State<PercentalSplitWidget> {
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       itemCount: widget.users.length,
-      separatorBuilder: (context, index) => const Gap(CustomPadding.mediumSpace),
+      separatorBuilder: (context, index) =>
+          const Gap(CustomPadding.mediumSpace),
       itemBuilder: (context, index) {
         return PercentTile(
           amount: widget.amount,
-          user: widget.users[index], // Pass the UserModel
+          user: widget.users[index],
           sliderValue: _sliderValues[index],
           onChanged: (value) => updateSlider(index, value),
         );
