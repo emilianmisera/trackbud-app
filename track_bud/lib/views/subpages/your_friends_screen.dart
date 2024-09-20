231e0fa360c1c6b4cc65d42223af36f3770daf90
Lenz <laurenz.ueckert@gmail.com>
2024-09-20 15:20:22 +0200

    load friends with right actual name and image

--- track_bud/lib/views/subpages/your_friends_screen.dart
+++ track_bud/lib/views/subpages/your_friends_screen.dart
@@ -1,18 +1,18 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:gap/gap.dart';
 import 'package:track_bud/models/user_model.dart';
+import 'package:track_bud/provider/user_provider.dart';
 import 'package:track_bud/services/invite_services.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/debts/friend/add_friend_bottom_sheet.dart';
 import 'package:track_bud/utils/debts/friend/friend_card.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfields/searchfield.dart';
-import 'package:track_bud/views/at_signup/firestore_service.dart';
 import 'package:share_plus/share_plus.dart';
 
 class YourFriendsScreen extends StatefulWidget {
-  const YourFriendsScreen({super.key});
+  const YourFriendsScreen({Key? key}) : super(key: key);
 
   @override
   State<YourFriendsScreen> createState() => _YourFriendsScreenState();
@@ -21,56 +21,29 @@ class YourFriendsScreen extends StatefulWidget {
 class _YourFriendsScreenState extends State<YourFriendsScreen> {
   final TextEditingController _searchController = TextEditingController();
   final InviteService _inviteService = InviteService();
-  final FirestoreService _firestoreService = FirestoreService();
-  List<UserModel> _friends = [];
-  bool _isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    _loadFriends();
-  }
-
-  String getCurrentUserId() {
-    User? currentUser = FirebaseAuth.instance.currentUser;
-    if (currentUser != null) {
-      return currentUser.uid; // Die eindeutige userId des aktuellen Benutzers
-    } else {
-      throw Exception('Kein Benutzer ist angemeldet');
-    }
-  }
-
-  Future<void> _loadFriends() async {
-    String currentUserId = getCurrentUserId();
-    try {
-      // Lade die Freunde des aktuellen Nutzers
-      List<UserModel> friends = await _firestoreService.getFriends(currentUserId);
-
-      setState(() {
-        _friends = friends;
-        _isLoading = false;
-      });
-    } catch (e) {
-      debugPrint('Fehler beim Laden der Freunde: $e');
-      setState(() {
-        _isLoading = false;
-      });
-    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<UserProvider>(context, listen: false).loadFriends();
+    });
   }
 
   Future<void> _shareInviteLink() async {
     try {
-      final userId = getCurrentUserId();
-      String inviteLink = await _inviteService.createInviteLink(userId);
-      Share.share('Füge mich zu deinen Freunden in TrackBud hinzu: $inviteLink');
+      final userId = Provider.of<UserProvider>(context, listen: false).currentUser?.userId;
+      if (userId != null) {
+        String inviteLink = await _inviteService.createInviteLink(userId);
+        Share.share('Füge mich zu deinen Freunden in TrackBud hinzu: $inviteLink');
+      }
     } catch (e) {
       debugPrint("Fehler beim Teilen des Links: $e");
     }
   }
 
   void _searchFriend(String query) {
-    //TODO: add search-function
-    // https://youtu.be/ZHdg2kfKmjI?si=ufWetKZ8HdE6OyjQ&t=49
+    // TODO: Implement search functionality
   }
 
   @override
@@ -80,48 +53,55 @@ class _YourFriendsScreenState extends State<YourFriendsScreen> {
         title: Text(AppTexts.yourFriends, style: TextStyles.regularStyleMedium),
         actions: [
           IconButton(
-              onPressed: () =>
-                  showModalBottomSheet(context: context, builder: (context) => AddFriendBottomSheet(onPressed: () => _shareInviteLink())),
-              icon: const Icon(
-                Icons.add,
-                color: CustomColor.bluePrimary,
-                size: 30,
-              ))
+            onPressed: () => showModalBottomSheet(
+              context: context,
+              builder: (context) => AddFriendBottomSheet(onPressed: _shareInviteLink),
+            ),
+            icon: const Icon(Icons.add, color: CustomColor.bluePrimary, size: 30),
+          )
         ],
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Padding(
-                // spacing between content and screen
-                padding: const EdgeInsets.only(
-                    top: CustomPadding.defaultSpace, left: CustomPadding.defaultSpace, right: CustomPadding.defaultSpace),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //SearchField
-                    SearchTextfield(
-                      hintText: AppTexts.search,
-                      controller: _searchController,
-                      onChanged: _searchFriend,
+      body: Consumer<UserProvider>(
+        builder: (context, userProvider, child) {
+          if (userProvider.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          
+          List<UserModel> friends = userProvider.friends;
+
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: CustomPadding.defaultSpace,
+                left: CustomPadding.defaultSpace,
+                right: CustomPadding.defaultSpace,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SearchTextfield(
+                    hintText: AppTexts.search,
+                    controller: _searchController,
+                    onChanged: _searchFriend,
+                  ),
+                  const Gap(CustomPadding.defaultSpace),
+                  if (friends.isEmpty)
+                    const Center(child: Text("Keine Freunde gefunden."))
+                  else
+                    Column(
+                      children: friends
+                          .map((friend) => Padding(
+                                padding: const EdgeInsets.symmetric(vertical: CustomPadding.smallSpace),
+                                child: FriendCard(friend: friend),
+                              ))
+                          .toList(),
                     ),
-                    const Gap(CustomPadding.defaultSpace),
-                    // List of Friends
-                    if (_friends.isEmpty)
-                      const Center(child: Text("Keine Freunde gefunden."))
-                    else
-                      Column(
-                        children: _friends
-                            .map((friend) => Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: CustomPadding.smallSpace),
-                                  child: FriendCard(friend: friend),
-                                ))
-                            .toList(),
-                      ),
-                  ],
-                ),
+                ],
               ),
             ),
+          );
+        },
+      ),
     );
   }
 }
