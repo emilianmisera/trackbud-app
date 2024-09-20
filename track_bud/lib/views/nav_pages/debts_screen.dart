f2fc597a8eb1c4e539496ef5494832c0a5261ef4
Lenz <laurenz.ueckert@gmail.com>
2024-09-20 15:17:22 +0200

    show the friends and groups on debtscreen

--- track_bud/lib/views/nav_pages/debts_screen.dart
+++ track_bud/lib/views/nav_pages/debts_screen.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:track_bud/models/user_model.dart';
+import 'package:provider/provider.dart';
+import 'package:track_bud/provider/group_provider.dart';
+import 'package:track_bud/provider/user_provider.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/debts/friend/friend_card.dart';
 import 'package:track_bud/utils/debts/group/group_card.dart';
-import 'package:track_bud/utils/tiles/information_tiles.dart';
 import 'package:track_bud/utils/strings.dart';
+import 'package:track_bud/utils/tiles/information_tiles.dart';
 import 'package:track_bud/views/subpages/your_friends_screen.dart';
 import 'package:track_bud/views/subpages/your_groups_screen.dart';
 
@@ -17,97 +20,164 @@ class DebtsScreen extends StatefulWidget {
 }
 
 class _DebtsScreenState extends State<DebtsScreen> {
-  // ignore: prefer_final_fields
-  List<UserModel> _friends = [];
-  // ignore: prefer_final_fields
-  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+    _loadGroups();
+  }
+
+  String getCurrentUserId() {
+    User? currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser != null) {
+      return currentUser.uid; // Die eindeutige userId des aktuellen Benutzers
+    } else {
+      throw Exception('Kein Benutzer ist angemeldet');
+    }
+  }
+
+  Future<void> _loadUserData() async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    await userProvider.loadCurrentUser();
+  }
+
+  Future<void> _loadGroups() async {
+    final groupProvider = Provider.of<GroupProvider>(context, listen: false);
+    await groupProvider
+        .loadGroups(); // Assume this method exists to load groups
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        // spacing between content and screen
-        padding: EdgeInsets.only(
-            top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
-            left: CustomPadding.defaultSpace,
-            right: CustomPadding.defaultSpace),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                InfoTile(
-                    title: AppTexts.debts,
-                    amount: 'amount',
-                    color: CustomColor.red,
-                    width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace),
-                InfoTile(
-                    title: AppTexts.credits,
-                    amount: 'amount',
-                    color: CustomColor.green,
-                    width: MediaQuery.sizeOf(context).width / 2 - Constants.infoTileSpace),
-              ],
-            ),
-            const Gap(CustomPadding.defaultSpace),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  AppTexts.friends,
-                  style: TextStyles.regularStyleMedium,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const YourFriendsScreen(),
-                      ),
-                    );
-                  },
-                  child: Text(AppTexts.showAll, style: TextStyles.regularStyleMedium.copyWith(color: CustomColor.bluePrimary)),
-                ),
-              ],
-            ),
-            const Gap(CustomPadding.mediumSpace),
-            //TODO: Use FutureBuilder instead of isLoading check
-            if (_isLoading)
-              const Center(child: CircularProgressIndicator())
-            else if (_friends.isEmpty)
-              const Center(child: Text("Keine Freunde gefunden."))
-            else
-              Column(
-                children: _friends
-                    .map((friend) => Column(
-                          children: [
-                            FriendCard(friend: friend),
-                            const Gap(CustomPadding.smallSpace), // Abstand zwischen den Karten
-                          ],
-                        ))
-                    .toList(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          // spacing between content and screen
+          padding: EdgeInsets.only(
+              top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace,
+              left: CustomPadding.defaultSpace,
+              right: CustomPadding.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InfoTile(
+                      title: AppTexts.debts,
+                      amount: 'amount',
+                      color: CustomColor.red,
+                      width: MediaQuery.sizeOf(context).width / 2 -
+                          Constants.infoTileSpace),
+                  InfoTile(
+                      title: AppTexts.credits,
+                      amount: 'amount',
+                      color: CustomColor.green,
+                      width: MediaQuery.sizeOf(context).width / 2 -
+                          Constants.infoTileSpace),
+                ],
               ),
-            const Gap(CustomPadding.defaultSpace),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(AppTexts.groups, style: TextStyles.regularStyleMedium),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const YourGroupsScreen(),
-                      ),
+              const Gap(
+                CustomPadding.defaultSpace,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppTexts.friends,
+                    style: TextStyles.regularStyleMedium,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const YourFriendsScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(AppTexts.showAll,
+                        style: TextStyles.regularStyleMedium
+                            .copyWith(color: CustomColor.bluePrimary)),
+                  ),
+                ],
+              ),
+              const Gap(
+                CustomPadding.mediumSpace,
+              ),
+              // Use Consumer to access friends from UserProvider
+              Consumer<UserProvider>(
+                builder: (context, userProvider, child) {
+                  if (userProvider.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (userProvider.friends.isEmpty) {
+                    return const Center(child: Text("Keine Freunde gefunden."));
+                  } else {
+                    return Column(
+                      children: userProvider.friends
+                          .take(5) // Limit to 5 friends
+                          .map((friend) => Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: CustomPadding.smallSpace),
+                                child: FriendCard(friend: friend),
+                              ))
+                          .toList(),
                     );
-                  },
-                  child: Text(AppTexts.showAll, style: TextStyles.regularStyleMedium.copyWith(color: CustomColor.bluePrimary)),
-                ),
-              ],
-            ),
-            const Gap(CustomPadding.mediumSpace),
-            const GroupCard()
-          ],
+                  }
+                },
+              ),
+              const Gap(
+                CustomPadding.defaultSpace,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppTexts.groups,
+                    style: TextStyles.regularStyleMedium,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const YourGroupsScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      AppTexts.showAll,
+                      style: TextStyles.regularStyleMedium
+                          .copyWith(color: CustomColor.bluePrimary),
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(CustomPadding.mediumSpace),
+              Consumer<GroupProvider>(
+                builder: (context, groupProvider, child) {
+                  if (groupProvider.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (groupProvider.groups.isEmpty) {
+                    return const Center(child: Text("Keine Gruppen gefunden."));
+                  } else {
+                    return Column(
+                      children: groupProvider.groups
+                          .take(3) //limits to 3
+                          .map((group) => Column(
+                                children: [
+                                  GroupCard(group: group),
+                                  const Gap(CustomPadding.smallSpace),
+                                ],
+                              ))
+                          .toList(),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
