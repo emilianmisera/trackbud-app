7e1dc63c7423d54f7d2ca477739fb253a0bd748e
Emilian <emilian.mi@web.de>
2024-09-21 09:50:31 +0400

    place MemberCategory Code to SplitTile and delete file

--- track_bud/lib/utils/tiles/split/split_tile.dart
+++ track_bud/lib/utils/tiles/split/split_tile.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:track_bud/utils/constants.dart';
-import 'package:track_bud/utils/enum/categories.dart';
 import 'package:track_bud/utils/shadow.dart';
-import 'package:track_bud/utils/tiles/split/member_category.dart';
 
 /// This Widget shows an added Split.
 /// It will be displayed in Group Overview Screen and in Friend Profile Screen.
 class SplitTile extends StatelessWidget {
   final Widget widget;
-  const SplitTile({super.key, required this.widget});
+  final String member;
+  final String? imageURL;
+  final String categoryName;
+  final Image categoryIcon;
+  final Color backgroundColor;
+  const SplitTile(
+      {super.key,
+      required this.widget,
+      required this.member,
+      this.imageURL,
+      required this.categoryName,
+      required this.categoryIcon,
+      required this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +33,25 @@ class SplitTile extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                MemberCategory(
-                  backgroundColor: Categories.lebensmittel.color,
-                  categoryIcon: Categories.lebensmittel.icon,
-                  categoryName: Categories.lebensmittel.name,
-                  member: 'Test',
+                 // information which user payed and in which category it has been assigned
+                Container(
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: backgroundColor),
+                  padding: const EdgeInsets.only(right: CustomPadding.defaultSpace),
+                  child: Row(
+                    children: [
+                      // Profile Picture
+                      const SizedBox(
+                        width: 25,
+                        height: 25,
+                        child: /* Image.network(imageURL, fit: BoxFit.cover) */ Icon(Icons.person, size: 25, color: Colors.grey),
+                      ),
+                      const Gap(CustomPadding.mediumSpace),
+                      // Category
+                      categoryIcon,
+                      const Gap(CustomPadding.smallSpace),
+                      Text(member, style: TextStyles.regularStyleDefault.copyWith(fontSize: TextStyles.fontSizeHint)),
+                    ],
+                  ),
                 ),
                 Text('125€', style: TextStyles.regularStyleMedium)
               ],
