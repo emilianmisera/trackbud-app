10eb66314bf97752fc4be1163f70df271d717720
Lenz <laurenz.ueckert@gmail.com>
2024-09-21 14:59:30 +0200

    add box to show total debts with friend

--- track_bud/lib/utils/debts/friend/friend_profile.dart
+++ track_bud/lib/utils/debts/friend/friend_profile.dart
@@ -1,33 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:track_bud/utils/constants.dart';
+import 'package:track_bud/utils/debts/balance_state.dart';
+import 'package:track_bud/utils/enum/debts_box.dart';
 import 'package:track_bud/utils/strings.dart';
 
-// Widget to display detailed information about a friend's debts and shared groups
 class FriendProfileDetails extends StatelessWidget {
+  final double totalDebt;
   const FriendProfileDetails({
     super.key,
+    required this.totalDebt,
   });
 
   @override
   Widget build(BuildContext context) {
+    // Determine the color scheme based on the totalDebt
+    DebtsColorScheme colorScheme;
+    if (totalDebt > 0) {
+      colorScheme = DebtsColorScheme.green;
+    } else if (totalDebt < 0) {
+      colorScheme = DebtsColorScheme.red;
+    } else {
+      colorScheme = DebtsColorScheme.blue;
+    }
+
     return Container(
       width: MediaQuery.sizeOf(context).width,
       decoration: BoxDecoration(
         color: CustomColor.white,
         borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
       ),
-      padding: const EdgeInsets.symmetric(horizontal: CustomPadding.defaultSpace, vertical: CustomPadding.defaultSpace),
+      padding: const EdgeInsets.symmetric(
+          horizontal: CustomPadding.defaultSpace,
+          vertical: CustomPadding.defaultSpace),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Debts section
-          Text(AppTexts.debts, style: TextStyles.regularStyleDefault),
-          const Gap(CustomPadding.mediumSpace),
-
-          // TODO: add Amount Box to display debt amount
-          // This should be implemented to show the current debt amount
-
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(AppTexts.debts, style: TextStyles.regularStyleDefault),
+              BalanceState(
+                colorScheme: colorScheme,
+                amount: totalDebt == 0
+                    ? null // Use null for 'quitt' to trigger the default in BalanceState
+                    : '${totalDebt.abs().toStringAsFixed(2)} €',
+              ),
+            ],
+          ),
           const Gap(CustomPadding.defaultSpace),
 
           // Shared groups section
@@ -35,7 +56,6 @@ class FriendProfileDetails extends StatelessWidget {
           const Gap(CustomPadding.mediumSpace),
 
           // TODO: add same Groups
-          // This should be implemented to show the groups shared with this friend
         ],
       ),
     );
