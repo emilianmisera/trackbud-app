09104feaa230e41211801e241d7ea3672dfe08de
Emilian <emilian.mi@web.de>
2024-09-21 09:32:49 +0400

    create MemberCategory Widget
    this widget shows the information which user payed and in which category it has been assigned

---
+++ track_bud/lib/utils/tiles/split/member_category.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:track_bud/utils/constants.dart';
+
+class MemberCategory extends StatelessWidget {
+  final String member;
+  final String? imageURL;
+  final String categoryName;
+  final Image categoryIcon;
+  final Color backgroundColor;
+  const MemberCategory(
+      {super.key,
+      required this.member,
+      this.imageURL,
+      required this.categoryName,
+      required this.categoryIcon,
+      required this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color: backgroundColor),
+      padding: const EdgeInsets.only(right: CustomPadding.defaultSpace),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 25,
+            height: 25,
+            child: /* Image.network(imageURL, fit: BoxFit.cover) */ Icon(Icons.person, size: 25, color: Colors.grey),
+          ),
+          const Gap(CustomPadding.mediumSpace),
+          categoryIcon,
+          const Gap(CustomPadding.smallSpace),
+          Text(member, style: TextStyles.regularStyleDefault.copyWith(fontSize: TextStyles.fontSizeHint)),
+        ],
+      ),
+    );
+  }
+}
