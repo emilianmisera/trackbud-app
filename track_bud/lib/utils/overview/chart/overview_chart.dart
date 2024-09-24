87ba2b817f9c9c7c60056d8f4d98ced72c572282
Lenz <laurenz.ueckert@gmail.com>
2024-09-24 23:33:03 +0200

    hide category amounts for now

--- track_bud/lib/utils/overview/chart/overview_chart.dart
+++ track_bud/lib/utils/overview/chart/overview_chart.dart
@@ -1,13 +1,16 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:provider/provider.dart';
+import 'package:track_bud/provider/user_provider.dart';
 import 'package:track_bud/utils/constants.dart';
-import 'package:track_bud/utils/enum/categories.dart';
 import 'package:track_bud/utils/debts/group/chart/group_debts_chart.dart';
-import 'package:track_bud/utils/overview/chart/expenses_chart.dart';
+import 'package:track_bud/utils/enum/categories.dart';
+import 'package:track_bud/utils/overview/chart/expenses_charts.dart';
 import 'package:track_bud/utils/shadow.dart';
 import 'package:track_bud/utils/tiles/time_unit_selection.dart';
+import 'package:track_bud/provider/transaction_provider.dart';
 
-// Main widget for displaying the expenses overview
 class ExpensesOverviewTile extends StatefulWidget {
   const ExpensesOverviewTile({super.key});
 
@@ -18,49 +21,14 @@ class ExpensesOverviewTile extends StatefulWidget {
 class _ExpensesOverviewTileState extends State<ExpensesOverviewTile> {
   int _currentTimeUnit = 1;
 
-  late List<double> _dailyExpenses;
-  late List<double> _weeklyExpenses;
-  late List<double> _monthlyExpenses;
-  late List<double> _yearlyExpenses;
-
   @override
   void initState() {
     super.initState();
-    _initializeExpenses();
-  }
-
-  void _initializeExpenses() {
-    _dailyExpenses = [100.00];
-    _weeklyExpenses = [100.00, 50.00, 20.00, 0.00, 0.00, 0, 0];
-    _monthlyExpenses = List.generate(31, (index) => index < 7 ? _weeklyExpenses[index] : 0);
-    _yearlyExpenses = List.generate(12, (index) => index == 0 ? _weeklyExpenses.reduce((a, b) => a + b) : 0);
-  }
-
-  Map<Categories, double> categoryAmounts = {
-    Categories.lebensmittel: 3.0,
-    Categories.drogerie: 2.0,
-    Categories.restaurant: 1.00,
-    Categories.mobility: 0.0,
-    Categories.shopping: 0.0,
-    Categories.unterkunft: 0.0,
-    Categories.entertainment: 0.0,
-    Categories.geschenk: 0.0,
-    Categories.sonstiges: 0.0,
-  };
-
-  List<double> _getCurrentExpenses() {
-    switch (_currentTimeUnit) {
-      case 0:
-        return _dailyExpenses;
-      case 1:
-        return _weeklyExpenses;
-      case 2:
-        return _monthlyExpenses;
-      case 3:
-        return _yearlyExpenses;
-      default:
-        return _weeklyExpenses;
-    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final transactionProvider = context.read<TransactionProvider>();
+      final userProvider = context.read<UserProvider>();
+      transactionProvider.calculateTotalExpenseForTimeUnit(_currentTimeUnit);
+    });
   }
 
   String _getTimeUnitText() {
@@ -68,7 +36,7 @@ class _ExpensesOverviewTileState extends State<ExpensesOverviewTile> {
       case 0:
         return 'Heute';
       case 1:
-        return 'Diese Woche';
+        return 'Die letzten 7 Tage';
       case 2:
         return 'Dieser Monat';
       case 3:
@@ -78,65 +46,83 @@ class _ExpensesOverviewTileState extends State<ExpensesOverviewTile> {
     }
   }
 
-  double _calculateTotalExpenses() {
-    return categoryAmounts.values.reduce((a, b) => a + b);
-  }
-
   @override
   Widget build(BuildContext context) {
-    final defaultColorScheme = Theme.of(context).colorScheme;
-    return CustomShadow(
-      child: Container(
-        padding: const EdgeInsets.all(CustomPadding.defaultSpace),
-        decoration: BoxDecoration(
-          color: defaultColorScheme.surface,
-          borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // select time unit
-            SelectTimeUnit(
-              onValueChanged: (int? newValue) {
-                setState(() {
-                  _currentTimeUnit = newValue ?? 1;
-                });
-              },
+    return Consumer<TransactionProvider>(
+      builder: (context, transactionProvider, child) {
+        if (transactionProvider.shouldReloadChart) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            transactionProvider
+                .calculateTotalExpenseForTimeUnit(_currentTimeUnit);
+            transactionProvider.resetReloadFlag();
+          });
+        }
+        final userProvider = context.read<UserProvider>();
+        final defaultColorScheme = Theme.of(context).colorScheme;
+        return CustomShadow(
+          child: Container(
+            padding: const EdgeInsets.all(CustomPadding.defaultSpace),
+            decoration: BoxDecoration(
+              color: defaultColorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(Constants.contentBorderRadius),
             ),
-            const Gap(CustomPadding.defaultSpace),
-            // Display current time period and total expense
-            Row(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: defaultColorScheme.secondary,
-                  size: 15,
+                SelectTimeUnit(
+                  onValueChanged: (int? newValue) {
+                    setState(() {
+                      _currentTimeUnit = newValue ?? 1;
+                    });
+                    context
+                        .read<TransactionProvider>()
+                        .calculateTotalExpenseForTimeUnit(_currentTimeUnit);
+                  },
                 ),
-                Text(
-                  _getTimeUnitText(),
-                  style: TextStyles.hintStyleDefault.copyWith(color: defaultColorScheme.secondary),
+                const Gap(CustomPadding.defaultSpace),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: defaultColorScheme.secondary,
+                      size: 15,
+                    ),
+                    Text(
+                      _getTimeUnitText(),
+                      style: TextStyles.hintStyleDefault,
+                    ),
+                  ],
                 ),
+                Consumer<TransactionProvider>(
+                  builder: (context, transactionProvider, child) {
+                    return Text(
+                      '${transactionProvider.totalExpenseForTimeUnit.toStringAsFixed(2)}€',
+                      style: TextStyles.headingStyle
+                          .copyWith(color: defaultColorScheme.primary),
+                    );
+                  },
+                ),
+                const Gap(CustomPadding.mediumSpace),
+                /* TransactionOverview(
+              isOverview: true,
+              categoryAmounts: context.watch<TransactionProvider>().categoryAmounts,
+            ),*/
+                const Gap(CustomPadding.defaultSpace),
+                Consumer<TransactionProvider>(
+                    builder: (context, transactionProvider, child) {
+                  return ExpensesCharts(
+                    currentTimeUnit: _currentTimeUnit,
+                    expenses: transactionProvider.expensesForTimeUnit,
+                    monthlyBudgetGoal:
+                        userProvider.currentUser?.monthlySpendingGoal ?? 0.0,
+                  );
+                })
               ],
             ),
-            Text(
-              _calculateTotalExpenses().toStringAsFixed(2),
-              style: TextStyles.headingStyle.copyWith(color: defaultColorScheme.primary),
-            ),
-            const Gap(CustomPadding.mediumSpace),
-            // Display overview of transactions by category
-            TransactionOverview(
-              isOverview: true,
-              categoryAmounts: categoryAmounts,
-            ),
-            const Gap(CustomPadding.defaultSpace),
-            // Display animated expenses chart
-            ExpensesChart(
-              currentTimeUnit: _currentTimeUnit,
-              expenses: _getCurrentExpenses(),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
