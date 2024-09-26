964b5cb8d5330ff565b8b116eba7ee3b78183e19
Emilian <emilian.mi@web.de>
2024-09-26 17:30:49 +0200

    GroupChoice layout and shadow fix

--- track_bud/lib/utils/debts/group/group_choice.dart
+++ track_bud/lib/utils/debts/group/group_choice.dart
@@ -12,10 +12,10 @@ class GroupChoice extends StatelessWidget {
   final FirestoreService _firestoreService = FirestoreService();
 
   GroupChoice({
-    Key? key,
+    super.key,
     required this.group,
     required this.onTap,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -23,24 +23,19 @@ class GroupChoice extends StatelessWidget {
     return CustomShadow(
       child: GestureDetector(
         onTap: () async {
-          List<String> memberNames =
-              await Future.wait(group.members.map((memberId) async {
+          List<String> memberNames = await Future.wait(group.members.map((memberId) async {
             UserModel? user = await _firestoreService.getUser(memberId);
             return user?.name ?? 'Unknown User';
           }));
           onTap(group, memberNames);
         },
-        child: Card(
-          color: defaultColorScheme.surface,
-          elevation: 2.0,
-          margin: const EdgeInsets.symmetric(
-            horizontal: CustomPadding.defaultSpace,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+        child: CustomShadow(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
+              color: defaultColorScheme.surface,
+            ),
+            padding: const EdgeInsets.all(CustomPadding.mediumSpace),
             child: Row(
               children: [
                 ClipRRect(
@@ -49,16 +44,13 @@ class GroupChoice extends StatelessWidget {
                     width: 40,
                     height: 40,
                     child: group.profilePictureUrl.isNotEmpty
-                        ? Image.network(group.profilePictureUrl,
-                            fit: BoxFit.cover)
+                        ? Image.network(group.profilePictureUrl, fit: BoxFit.cover)
                         : const Icon(Icons.group, color: Colors.grey),
                   ),
                 ),
                 const Gap(CustomPadding.mediumSpace),
                 Expanded(
-                  child: Text(group.name,
-                      style: TextStyles.regularStyleMedium
-                          .copyWith(color: defaultColorScheme.primary)),
+                  child: Text(group.name, style: TextStyles.regularStyleMedium.copyWith(color: defaultColorScheme.primary)),
                 ),
                 SizedBox(
                   width: 65,
@@ -68,8 +60,7 @@ class GroupChoice extends StatelessWidget {
                       return FutureBuilder<UserModel?>(
                         future: _firestoreService.getUser(group.members[index]),
                         builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
+                          if (snapshot.connectionState == ConnectionState.waiting) {
                             return const SizedBox.shrink();
                           }
                           if (snapshot.hasError || snapshot.data == null) {
@@ -84,10 +75,8 @@ class GroupChoice extends StatelessWidget {
                                 width: 30,
                                 height: 30,
                                 child: member.profilePictureUrl.isNotEmpty
-                                    ? Image.network(member.profilePictureUrl,
-                                        fit: BoxFit.cover)
-                                    : const Icon(Icons.person,
-                                        color: Colors.grey),
+                                    ? Image.network(member.profilePictureUrl, fit: BoxFit.cover)
+                                    : const Icon(Icons.person, color: Colors.grey),
                               ),
                             ),
                           );
