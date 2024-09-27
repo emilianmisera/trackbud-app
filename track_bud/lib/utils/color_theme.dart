6af398ad8e3740ab0c5b91875daa04cc981f10fe
Lenz <laurenz.ueckert@gmail.com>
2024-09-28 01:34:19 +0200

    add surfaceHint that is used to highlight small things in both colorSchemes for better contrast

--- track_bud/lib/utils/color_theme.dart
+++ track_bud/lib/utils/color_theme.dart
@@ -18,34 +18,48 @@ class ColorTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       //style for elevated button
       style: ButtonStyle(
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
-        backgroundColor: WidgetStateProperty.resolveWith<Color>(
-            (states) => states.contains(WidgetState.disabled) ? CustomColor.bluePrimary.withOpacity(0.5) : CustomColor.bluePrimary),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
+        backgroundColor: WidgetStateProperty.resolveWith<Color>((states) =>
+            states.contains(WidgetState.disabled)
+                ? CustomColor.bluePrimary.withOpacity(0.5)
+                : CustomColor.bluePrimary),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
         textStyle: WidgetStateProperty.all(TextStyles.buttonTextStyle),
         foregroundColor: WidgetStateProperty.resolveWith<Color>(
-          (states) => states.contains(WidgetState.disabled) ? CustomColor.white.withOpacity(0.5) : CustomColor.white, // Default text color
+          (states) => states.contains(WidgetState.disabled)
+              ? CustomColor.white.withOpacity(0.5)
+              : CustomColor.white, // Default text color
         ),
       ),
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       //style for outlinedButton
       style: ButtonStyle(
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
         foregroundColor: WidgetStateProperty.all(CustomColor.black),
-        side: WidgetStateProperty.all(const BorderSide(color: CustomColor.hintColor, width: 2.0, style: BorderStyle.solid)),
-        backgroundColor: WidgetStateProperty.all(CustomColor.backgroundPrimary), // when not pressing
+        side: WidgetStateProperty.all(const BorderSide(
+            color: CustomColor.hintColor,
+            width: 2.0,
+            style: BorderStyle.solid)),
+        backgroundColor: WidgetStateProperty.all(
+            CustomColor.backgroundPrimary), // when not pressing
         textStyle: WidgetStateProperty.all(TextStyles.regularStyleMedium),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
       ),
     ),
@@ -57,43 +71,52 @@ class ColorTheme {
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
       ),
     ),
     colorScheme: const ColorScheme.light(
-        tertiary: CustomColor.pastelBlue,
-        onTertiary: CustomColor.pastelGreen,
-        tertiaryContainer: CustomColor.pastelRed,
-        // Text
-        primary: CustomColor.black,
-        //Hint Text
-        secondary: CustomColor.hintColor,
-        //background tiles
-        surface: CustomColor.white,
-        // background Color
-        onSurface: CustomColor.backgroundPrimary,
-        // grey Color
-        outline: CustomColor.grey),
+      tertiary: CustomColor.pastelBlue,
+      onTertiary: CustomColor.pastelGreen,
+      tertiaryContainer: CustomColor.pastelRed,
+      // Text
+      primary: CustomColor.black,
+      //Hint Text
+      secondary: CustomColor.hintColor,
+      //background tiles
+      surface: CustomColor.white,
+      // background Color
+      onSurface: CustomColor.backgroundPrimary,
+      // grey Color
+      outline: CustomColor.grey,
+      // for small elements where primary blue is to dark in darkmode
+      surfaceTint: CustomColor.bluePrimary,
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
     colorScheme: const ColorScheme.dark(
-        tertiary: CustomColor.darkModePastelBlue,
-        onTertiary: CustomColor.darkModePastelGreen,
-        tertiaryContainer: CustomColor.darkModePastelRed,
-        // Text
-        primary: CustomColor.darkModeWhite,
-        //Hint Text
-        secondary: CustomColor.darkModeHint,
-        //background tiles
-        surface: CustomColor.darkModeBlack,
-        // background Color
-        onSurface: CustomColor.darkModebackgroundPrimary,
-        // grey Color
-        outline: CustomColor.darkModeGrey),
+      tertiary: CustomColor.darkModePastelBlue,
+      onTertiary: CustomColor.darkModePastelGreen,
+      tertiaryContainer: CustomColor.darkModePastelRed,
+      // Text
+      primary: CustomColor.darkModeWhite,
+      //Hint Text
+      secondary: CustomColor.darkModeHint,
+      //background tiles
+      surface: CustomColor.darkModeBlack,
+      // background Color
+      onSurface: CustomColor.darkModebackgroundPrimary,
+      // grey Color
+      outline: CustomColor.darkModeGrey,
+      // for small elements where primary blue is to dark in darkmode
+      surfaceTint: CustomColor.darkModeWhite,
+    ),
     brightness: Brightness.dark,
     useMaterial3: true,
     scaffoldBackgroundColor: CustomColor.darkModebackgroundPrimary,
@@ -106,34 +129,49 @@ class ColorTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       //style for elevated button
       style: ButtonStyle(
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
-        backgroundColor: WidgetStateProperty.resolveWith<Color>(
-            (states) => states.contains(WidgetState.disabled) ? CustomColor.bluePrimary.withOpacity(0.5) : CustomColor.bluePrimary),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
+        backgroundColor: WidgetStateProperty.resolveWith<Color>((states) =>
+            states.contains(WidgetState.disabled)
+                ? CustomColor.bluePrimary.withOpacity(0.5)
+                : CustomColor.bluePrimary),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
         textStyle: WidgetStateProperty.all(TextStyles.buttonTextStyle),
         foregroundColor: WidgetStateProperty.resolveWith<Color>(
-          (states) => states.contains(WidgetState.disabled) ? CustomColor.white.withOpacity(0.5) : CustomColor.white, // Default text color
+          (states) => states.contains(WidgetState.disabled)
+              ? CustomColor.white.withOpacity(0.5)
+              : CustomColor.white, // Default text color
         ),
       ),
     ),
     outlinedButtonTheme: OutlinedButtonThemeData(
       //style for outlinedButton
       style: ButtonStyle(
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
         foregroundColor: WidgetStateProperty.all(CustomColor.black),
-        side: WidgetStateProperty.all(const BorderSide(color: CustomColor.darkModeHint, width: 2.0, style: BorderStyle.solid)),
-        backgroundColor: WidgetStateProperty.all(CustomColor.darkModebackgroundPrimary), // when not pressing
-        textStyle: WidgetStateProperty.all(TextStyles.regularStyleMedium.copyWith(color: CustomColor.darkModeWhite)),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        side: WidgetStateProperty.all(const BorderSide(
+            color: CustomColor.darkModeHint,
+            width: 2.0,
+            style: BorderStyle.solid)),
+        backgroundColor: WidgetStateProperty.all(
+            CustomColor.darkModebackgroundPrimary), // when not pressing
+        textStyle: WidgetStateProperty.all(TextStyles.regularStyleMedium
+            .copyWith(color: CustomColor.darkModeWhite)),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
       ),
     ),
@@ -141,13 +179,17 @@ class ColorTheme {
       style: ButtonStyle(
         backgroundColor: WidgetStateProperty.all(CustomColor.darkModeBlack),
         foregroundColor: WidgetStateProperty.all(CustomColor.darkModeWhite),
-        textStyle: WidgetStateProperty.all(TextStyles.regularStyleMedium.copyWith(color: CustomColor.darkModeWhite)),
+        textStyle: WidgetStateProperty.all(TextStyles.regularStyleMedium
+            .copyWith(color: CustomColor.darkModeWhite)),
         shape: WidgetStateProperty.all(RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.contentBorderRadius),
         )),
-        minimumSize: WidgetStateProperty.all(const Size(double.infinity, Constants.height)),
-        overlayColor:
-            WidgetStateProperty.resolveWith<Color?>((states) => states.contains(WidgetState.pressed) ? CustomColor.overlayColor : null),
+        minimumSize: WidgetStateProperty.all(
+            const Size(double.infinity, Constants.height)),
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) =>
+            states.contains(WidgetState.pressed)
+                ? CustomColor.overlayColor
+                : null),
         animationDuration: Constants.buttonAnimationDuration,
       ),
     ),
