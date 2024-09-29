db035b4793a16f13e346f3c0cf0e7a58ae28410f
Lenz <laurenz.ueckert@gmail.com>
2024-09-29 14:59:47 +0200

    add comments

--- track_bud/lib/views/nav_pages/overview_screen.dart
+++ track_bud/lib/views/nav_pages/overview_screen.dart
@@ -6,12 +6,12 @@ import 'package:track_bud/provider/transaction_provider.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/overview/monthly_expense.dart';
 import 'package:track_bud/utils/overview/chart/overview_chart.dart';
-import 'package:track_bud/utils/overview/debts.dart';
+import 'package:track_bud/utils/overview/overview_debts_chart.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/widgets/transaction_list.dart';
 
 class OverviewScreen extends StatefulWidget {
-  const OverviewScreen({Key? key}) : super(key: key);
+  const OverviewScreen({super.key});
 
   @override
   State<OverviewScreen> createState() => _OverviewScreenState();
@@ -21,10 +21,14 @@ class _OverviewScreenState extends State<OverviewScreen> {
   @override
   void initState() {
     super.initState();
+    _initializeProviders();
+  }
+
+  /// Initializes the user and transaction providers after the first frame.
+  void _initializeProviders() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final userProvider = Provider.of<UserProvider>(context, listen: false);
-      final transactionProvider =
-          Provider.of<TransactionProvider>(context, listen: false);
+      final transactionProvider = Provider.of<TransactionProvider>(context, listen: false);
 
       userProvider.loadCurrentUser();
       transactionProvider.initializeBalance();
@@ -35,6 +39,7 @@ class _OverviewScreenState extends State<OverviewScreen> {
   @override
   Widget build(BuildContext context) {
     final defaultColorScheme = Theme.of(context).colorScheme;
+
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(
@@ -45,19 +50,18 @@ class _OverviewScreenState extends State<OverviewScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const ExpensesOverviewTile(),
+            const ExpensesOverviewTile(), // Tile for overall expenses
             const Gap(CustomPadding.defaultSpace),
-            const MonthlyExpenseTile(),
+            const MonthlyExpenseTile(), // Tile for monthly expenses
             const Gap(CustomPadding.defaultSpace),
-            const OverviewDebtsTile(),
+            const OverviewDebtsTile(), // Tile for overview of debts
             const Gap(CustomPadding.defaultSpace),
             Text(
-              AppTexts.history,
-              style: TextStyles.regularStyleMedium
-                  .copyWith(color: defaultColorScheme.primary),
+              AppTexts.history, // Title for transaction history
+              style: TextStyles.regularStyleMedium.copyWith(color: defaultColorScheme.primary),
             ),
             const Gap(CustomPadding.mediumSpace),
-            const TransactionHistoryList(transactionType: 'expense')
+            const TransactionHistoryList(transactionType: 'expense'), // List for transaction history
           ],
         ),
       ),
