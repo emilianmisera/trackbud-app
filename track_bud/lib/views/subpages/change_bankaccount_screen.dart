7b8f5148cc10d021a267d506b01ee0129cb08c31
Lenz <laurenz.ueckert@gmail.com>
2024-09-29 15:00:17 +0200

    outsourced methods to user_provider

--- track_bud/lib/views/subpages/change_bankaccount_screen.dart
+++ track_bud/lib/views/subpages/change_bankaccount_screen.dart
@@ -1,116 +1,55 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:gap/gap.dart';
+import 'package:track_bud/provider/user_provider.dart';
 import 'package:track_bud/utils/constants.dart';
 import 'package:track_bud/utils/strings.dart';
 import 'package:track_bud/utils/textfields/textfield_amount_of_money.dart';
 
 class ChangeBankaccountScreen extends StatefulWidget {
-  const ChangeBankaccountScreen({super.key});
+  const ChangeBankaccountScreen({Key? key}) : super(key: key);
 
   @override
   State<ChangeBankaccountScreen> createState() => _ChangeBankaccountScreenState();
 }
 
 class _ChangeBankaccountScreenState extends State<ChangeBankaccountScreen> {
-  // Controller to handle the input in the TextField for the amount of money.
   final TextEditingController _moneyController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _loadCurrentBankAccountInfo(); // Load bank account info when screen is initialized
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _loadCurrentBankAccountInfo();
+    });
   }
 
-  Future<void> _loadCurrentBankAccountInfo() async {
-    final defaultColorScheme = Theme.of(context).colorScheme;
-    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-
-    if (userId.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Benutzer nicht angemeldet.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-        ),
-      );
-      return;
-    }
-
-    try {
-      // Fetch user data directly from Firestore
-      final userDoc = await FirebaseFirestore.instance.collection('users').doc(userId).get();
-
-      if (userDoc.exists) {
-        final userData = userDoc.data();
-        if (userData != null && userData.containsKey('bankAccountBalance')) {
-          setState(() {
-            _moneyController.text = userData['bankAccountBalance'].toStringAsFixed(2);
-          });
-        } else {
-          // Handle case where bank account balance is not found in Firestore
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content:
-                  Text("Bankkontostand nicht gefunden.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-            ),
-          );
-        }
-      } else {
-        // Handle case where user document is not found in Firestore
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content:
-                Text("Benutzerdaten nicht gefunden.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-          ),
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Fehler beim Laden des Bankkontostands: $e", style: TextStyle(color: defaultColorScheme.primary)),
-        ),
-      );
-    }
+  void _loadCurrentBankAccountInfo() {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final currentBankBalance = userProvider.currentUser?.bankAccountBalance ?? 0.0;
+    setState(() {
+      _moneyController.text = currentBankBalance.toStringAsFixed(2);
+    });
   }
 
   Future<void> _saveBankAccountInfo() async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
     final defaultColorScheme = Theme.of(context).colorScheme;
-    final String userId = FirebaseAuth.instance.currentUser?.uid ?? '';
-
-    if (userId.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Benutzer nicht angemeldet.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-        ),
-      );
-      return;
-    }
 
     final String amountText = _moneyController.text.trim().replaceAll(',', '.');
     if (amountText.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content:
-              Text("Bitte geben Sie den Betrag ein.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-        ),
-      );
+      _showErrorSnackBar("Bitte geben Sie den Betrag ein.");
       return;
     }
 
     final double amount = double.tryParse(amountText) ?? -1;
     if (amount < 0) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Ungültiger Betrag.", style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-        ),
-      );
+      _showErrorSnackBar("Ungültiger Betrag.");
       return;
     }
 
     try {
-      // Update bank account balance directly in Firestore
-      await FirebaseFirestore.instance.collection('users').doc(userId).update({'bankAccountBalance': amount});
-
+      await userProvider.setBankAccountBalance(amount);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text("Bankkonto erfolgreich aktualisiert.",
@@ -119,13 +58,17 @@ class _ChangeBankaccountScreenState extends State<ChangeBankaccountScreen> {
       );
       Navigator.pop(context);
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Fehler beim Speichern der Bankkonto-Informationen: $e",
-              style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
-        ),
-      );
+      _showErrorSnackBar("Fehler beim Speichern der Bankkonto-Informationen: $e");
     }
+  }
+
+  void _showErrorSnackBar(String message) {
+    final defaultColorScheme = Theme.of(context).colorScheme;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message, style: TextStyles.regularStyleDefault.copyWith(color: defaultColorScheme.primary)),
+      ),
+    );
   }
 
   @override
@@ -135,31 +78,24 @@ class _ChangeBankaccountScreenState extends State<ChangeBankaccountScreen> {
       appBar: AppBar(),
       body: SingleChildScrollView(
         child: Padding(
-          // Padding adds spacing around the content inside the screen.
           padding: EdgeInsets.only(
-            top: MediaQuery.sizeOf(context).height * CustomPadding.topSpace -
-                Constants.defaultAppBarHeight, // Top padding based on screen height
-            left: CustomPadding.defaultSpace, // Left padding
-            right: CustomPadding.defaultSpace, // Right padding
+            top: MediaQuery.of(context).size.height * CustomPadding.topSpace - Constants.defaultAppBarHeight,
+            left: CustomPadding.defaultSpace,
+            right: CustomPadding.defaultSpace,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                AppTexts.changeBankAccHeading, // The heading text
-                style: TextStyles.headingStyle.copyWith(color: defaultColorScheme.primary), // The text style for the heading.
+                AppTexts.changeBankAccHeading,
+                style: TextStyles.headingStyle.copyWith(color: defaultColorScheme.primary),
               ),
-              const Gap(
-                CustomPadding.mediumSpace, // Adds vertical space between the heading and the next element.
-              ),
+              const Gap(CustomPadding.mediumSpace),
               Text(
-                AppTexts.changeBankAccDescribtion, // The description text
-                style: TextStyles.hintStyleDefault, // The text style for the description.
+                AppTexts.changeBankAccDescribtion,
+                style: TextStyles.hintStyleDefault,
               ),
-              const Gap(
-                CustomPadding.bigSpace, // Adds more vertical space before the next element.
-              ),
-              // A custom TextField widget for entering the amount of money, using the controller defined above.
+              const Gap(CustomPadding.bigSpace),
               TextFieldAmountOfMoney(
                 controller: _moneyController,
                 hintText: AppTexts.lines,
@@ -171,18 +107,15 @@ class _ChangeBankaccountScreenState extends State<ChangeBankaccountScreen> {
       bottomSheet: Container(
         color: defaultColorScheme.onSurface,
         child: Container(
-          // Margin is applied to the bottom of the button and the sides for proper spacing.
           margin: EdgeInsets.only(
-            bottom: MediaQuery.sizeOf(context).height * CustomPadding.bottomSpace, // Bottom margin based on screen height
-            left: CustomPadding.defaultSpace, // Left margin
-            right: CustomPadding.defaultSpace, // Right margin
+            bottom: MediaQuery.of(context).size.height * CustomPadding.bottomSpace,
+            left: CustomPadding.defaultSpace,
+            right: CustomPadding.defaultSpace,
           ),
+          width: MediaQuery.of(context).size.width,
           child: ElevatedButton(
-            // Saving Button
             onPressed: _saveBankAccountInfo,
-            child: Text(
-              AppTexts.save,
-            ),
+            child: Text(AppTexts.save),
           ),
         ),
       ),
