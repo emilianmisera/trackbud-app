0b8c073e541b43e235881edf5e979e141ece5560
Emilian <emilian.mi@web.de>
2024-09-30 12:18:13 +0200

    remove unsued class

--- track_bud/lib/utils/textinput_formatters.dart
+++ track_bud/lib/utils/textinput_formatters.dart
@@ -2,36 +2,6 @@
 
 import 'package:flutter/services.dart';
 
-class GermanNumericTextFormatter extends TextInputFormatter {
-  @override
-  TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue, TextEditingValue newValue) {
-    if (newValue.text.isEmpty) {
-      return newValue.copyWith(text: '');
-    }
-
-    // only numbers and comma
-    String newText = newValue.text.replaceAll(RegExp(r'[^0-9,]'), '');
-
-    // only one comma
-    if (newText.split(',').length > 2) {
-      return oldValue;
-    }
-
-    // only two decimal places
-    var parts = newText.split(',');
-    if (parts.length > 1 && parts[1].length > 2) {
-      parts[1] = parts[1].substring(0, 2);
-      newText = parts.join(',');
-    }
-
-    return TextEditingValue(
-      text: newText,
-      selection: TextSelection.collapsed(offset: newText.length),
-    );
-  }
-}
-
 class MaxValueInputFormatter extends TextInputFormatter {
   final double maxValue;
 
